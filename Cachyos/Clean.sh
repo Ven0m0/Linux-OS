6df2a0f5a53fda9a37ba988c175f74840205e260
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-08 05:41:14 +0200

    Update Clean.sh

--- Cachyos/Clean.sh
+++ Cachyos/Clean.sh
@@ -1,60 +1,60 @@
-echo '--- Clear system-wide cache'
+# Clear system-wide cache
 rm -rf /var/cache/*
-sudo rm -rfv /tmp/*
-sudo rm -rfv /var/tmp/*
-sudo rm -rfv /var/crash/*
-sudo rm -rfv /var/lib/systemd/coredump/
-echo '--- Empty trash'
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
+sudo rm -rf /var/crash/*
+sudo rm -rf /var/lib/systemd/coredump/
 # Empty global trash
-rm -rfv ~/.local/share/Trash/*
-sudo rm -rfv /root/.local/share/Trash/*
+rm -rf ~/.local/share/Trash/*
+sudo rm -rf /root/.local/share/Trash/*
 echo '--- Clear user-specific cache'
-rm -rfv ~/.cache/*
-sudo rm -rfv root/.cache/*
-rm -fv ~/.mozilla/firefox/Crash\ Reports/*
+rm -rf ~/.cache/*
+sudo rm -rf root/.cache/*
+rm -f ~/.mozilla/firefox/Crash\ Reports/*
 echo '--- Clear Flatpak cache'
-rm -rfv ~/.var/app/*/cache/*
-sudo rm -rfv /var/tmp/flatpak-cache-*
-rm -rfv ~/.cache/flatpak/system-cache/*
-rm -rfv ~/.local/share/flatpak/system-cache/*
-rm -rfv ~/.var/app/*/data/Trash/*
+rm -rf ~/.var/app/*/cache/*
+sudo rm -rf /var/tmp/flatpak-cache-*
+rm -rf ~/.cache/flatpak/system-cache/*
+rm -rf ~/.local/share/flatpak/system-cache/*
+rm -rf ~/.var/app/*/data/Trash/*
 echo '--- Clear Snap cache'
-rm -fv ~/snap/*/*/.cache/*
-sudo rm -rfv /var/lib/snapd/cache/*
-rm -rfv ~/snap/*/*/.local/share/Trash/*
+rm -f ~/snap/*/*/.cache/*
+sudo rm -rf /var/lib/snapd/cache/*
+rm -rf ~/snap/*/*/.local/share/Trash/*
 echo '--- Clear thumbnails (icon cache)'
-rm -rfv ~/.thumbnails/*
-rm -rfv ~/.cache/thumbnails/*
-echo '--- Clear system logs (`journald`)'
+rm -rf ~/.thumbnails/*
+rm -rf ~/.cache/thumbnails/*
+# Clear system logs
+sudo rm -f /var/log/pacman.log
 sudo journalctl --vacuum-time=1s
-sudo rm -rfv /run/log/journal/*
-sudo rm -rfv /var/log/journal/*
+sudo rm -rf /run/log/journal/*
+sudo rm -rf /var/log/journal/*
 # Terminal
-rm -fv ~/.local/share/fish/fish_history
-sudo rm -fv /root/.local/share/fish/fish_history
-rm -fv ~/.config/fish/fish_history
-sudo rm -fv /root/.config/fish/fish_history
-rm -fv ~/.zsh_history
-sudo rm -fv /root/.zsh_history
-rm -fv ~/.bash_history
-sudo rm -fv /root/.bash_history
+rm -f ~/.local/share/fish/fish_history
+sudo rm -f /root/.local/share/fish/fish_history
+rm -f ~/.config/fish/fish_history
+sudo rm -f /root/.config/fish/fish_history
+rm -f ~/.zsh_history
+sudo rm -f /root/.zsh_history
+rm -f ~/.bash_history
+sudo rm -f /root/.bash_history
 # LibreOffice
 rm -f ~/.config/libreoffice/4/user/registrymodifications.xcu
-rm -fv ~/snap/libreoffice/*/.config/libreoffice/4/user/registrymodifications.xcu
-rm -fv ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu
+rm -f ~/snap/libreoffice/*/.config/libreoffice/4/user/registrymodifications.xcu
+rm -f ~/.var/app/org.libreoffice.LibreOffice/config/libreoffice/4/user/registrymodifications.xcu
 # Steam
-rm -rfv ~/.local/share/Steam/appcache/*
-rm -rfv ~/snap/steam/common/.cache/*
-rm -rfv ~/snap/steam/common/.local/share/Steam/appcache/*
-rm -rfv ~/.var/app/com.valvesoftware.Steam/cache/*
-rm -rfv ~/.var/app/com.valvesoftware.Steam/data/Steam/appcache/*
+rm -rf ~/.local/share/Steam/appcache/*
+rm -rf ~/snap/steam/common/.cache/*
+rm -rf ~/snap/steam/common/.local/share/Steam/appcache/*
+rm -rf ~/.var/app/com.valvesoftware.Steam/cache/*
+rm -rf ~/.var/app/com.valvesoftware.Steam/data/Steam/appcache/*
 # Python
-rm -fv ~/.python_history
-sudo rm -fv /root/.python_history
+rm -f ~/.python_history
+sudo rm -f /root/.python_history
 # Wine
-rm -rfv ~/.wine/drive_c/windows/temp/*
-rm -rfv ~/.cache/wine/
-rm -rfv ~/.cache/winetricks/
+rm -rf ~/.wine/drive_c/windows/temp/*
+rm -rf ~/.cache/wine/
+rm -rf ~/.cache/winetricks/
 # My stuff
 profile-cleaner f
 paccache -r

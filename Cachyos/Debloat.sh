e10aebe054f61d881c28c5a21ced8aa848c3eeee
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-08 05:22:23 +0200

    Update Debloat.sh

--- Cachyos/Debloat.sh
+++ Cachyos/Debloat.sh
@@ -1,4 +1,24 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]; then
+  script_path=$([[ "$0" = /* ]] && echo "$0" || echo "$PWD/${0#./}")
+  sudo "$script_path" || (
+    echo 'Administrator privileges are required.'
+    exit 1
+  )
+  exit 0
+fi
+export HOME="/home/${SUDO_USER:-${USER}}"
+
+
+# Mostly useless
 pacman -Rns kcontacts
 pacman -Rns kdeconnect
 pacman -Rns kpeople
 pacman -Rns plasma-browser-integration
+# Deprecated
+pacman -Rns cachy-browser
+
+
+echo 'The script finished. Press any key to exit.'
+read -n 1 -s

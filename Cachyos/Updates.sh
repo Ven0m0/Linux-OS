6c583163644cf6a5484e317ddf0db86644b97faa
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-14 04:26:58 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
-  script_path=$([[ "$0" = /* ]] && echo "$0" || echo "$PWD/${0#./}")
-  sudo "$script_path" || (
-    echo 'Administrator privileges are required.'
-    exit 1
-  )
-  exit 0
-fi
-export HOME="/home/${SUDO_USER:-${USER}}"
-
 sudo pacman -Syu --noconfirm
 sudo topgrade -c --disable config_update --skip-notify -y

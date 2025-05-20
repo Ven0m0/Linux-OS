d601aa7d94fb2209a50f5a32416f28327c021d3c
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-20 03:36:48 +0200

    Add files via upload

---
+++ RaspberryPi/PiClean.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+sudo apt clean && sudo apt autoclean && sudo apt-get -y autoremove --purge
+sudo rm -rfv /var/lib/apt/lists/*
+sudo pip cache purge
+rm -rf /var/cache/*
+rm -rfv ~/.cache/*
+sudo rm -rfv root/.cache/*
+rm -rfv ~/.thumbnails/*
+rm -rfv ~/.cache/thumbnails/*
+sudo rm -rfv /tmp/*
+sudo rm -rfv /var/tmp/*
+sudo rm -rf /var/crash/*
+sudo rm -rf /var/lib/systemd/coredump/
+rm -rfv ~/.local/share/Trash/*
+sudo rm -rfv /root/.local/share/Trash/*
+rm -fv ~/.python_history
+sudo rm -fv /root/.python_history
+rm -fv ~/.bash_history
+sudo rm -fv /root/.bash_history
+if ! command -v 'journalctl' &> /dev/null; then
+  echo 'Skipping because "journalctl" is not found.'
+else
+  sudo journalctl --vacuum-time=1s
+fi
+sudo rm -rfv /run/log/journal/*
+sudo rm -rfv /var/log/journal/*
+sudo fstrim -av --quiet-unsupported
+if ! command -v 'localepurge' &> /dev/null; then
+  echo 'Skipping because "localepurge" is not found.'
+else
+  sudo localepurge
+fi

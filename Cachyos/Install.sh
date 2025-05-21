591ec9018d7f6ac77957b2e73c68220c13b23145
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-21 16:54:37 +0200

    Update Install.sh

--- Cachyos/Install.sh
+++ Cachyos/Install.sh
@@ -2,14 +2,16 @@
 
 sudo -v
 
-sudo pacman -S bauh
+sudo cachyos-rate-mirrors
+sudo pacman -S keyserver-rank-cachy
+sudo keyserver-rank --yes
+
 
 # List of packages to install
 packages=(
 topgrade
 bauh
 partitionmanager
-keyserver-rank-cachy
 modprobed-db
 rustup
 llvm-bolt
@@ -25,6 +27,11 @@ profile-sync-daemon
 bleachbit
 irqbalance
 aria2
+winesync-udev-rule
+legcord
+vkd3d
+obs-studio
+mkinitcpio-firmware
 )
 
 echo -e "\nInstalling packages: ${packages[*]}"
@@ -45,3 +52,19 @@ cleanerml-git
 alhp-keyring
 alhp-mirrorlist
 makepkg-optimize
+
+
+rustup toolchain install nightly --profile minimal
+rustup component add rust-src
+rustup component add llvm-tools-x86_64-unknown-linux-gnu
+rustup component add rust-std-x86_64-unknown-linux-musl
+rustup component add rustfmt-x86_64-unknown-linux-gnu
+rustup component add rustc-dev-x86_64-unknown-linux-gnu
+
+sudo pacman -Rns cachyos-v4-mirrorlist
+sudo pacman -Rns cachy-browser
+
+
+sudo systemctl enable fstrim.timer
+sudo pacman -Syyu --noconfirm
+sudo topgrade -c --disable config_update --skip-notify -y

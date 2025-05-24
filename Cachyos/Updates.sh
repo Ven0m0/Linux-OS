fa17dd4a52cf52f38739d7ba2d480a7d993e0d07
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-24 04:13:53 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo -v
+
 sudo pacman -Syu --noconfirm && sudo topgrade -c --disable config_update --skip-notify -y
 if bootctl is-systemd-boot | grep -q 'yes'; then
     bootctl update

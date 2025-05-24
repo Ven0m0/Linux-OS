470a9d92910d609af21c27d7189e7f88da768cbc
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-24 04:20:17 +0200

    Update Clean.sh

--- Cachyos/Clean.sh
+++ Cachyos/Clean.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
-  script_path=$([[ "$0" = /* ]] && echo "$0" || echo "$PWD/${0#./}")
-  sudo "$script_path" || (
-    echo 'Administrator privileges are required.'
-    exit 1
-  )
-  exit 0
-fi
-export HOME="/home/${SUDO_USER:-${USER}}"
+sudo -v
 
 # Clear system-wide cache
 rm -rf /var/cache/*

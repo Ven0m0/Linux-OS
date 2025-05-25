1a8efd13e9c65bfe82587be90ef7159b85cddc29
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-25 20:30:03 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update --allow-releaseinfo-change && sudo apt-get dist-upgrade -y && sudo apt full-upgrade -y
-if command -v dietpi-launcher > /dev/null; then
+if command -v dietpi-config > /dev/null; then
     sudo dietpi-update
 else
     echo "system is not dietpi"

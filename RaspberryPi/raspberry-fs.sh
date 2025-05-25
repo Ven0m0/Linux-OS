120dfe792a840105f0b3e2aa974020e2f9dc5c73
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-25 21:39:46 +0200

    Update raspberry-fs.sh

--- RaspberryPi/raspberry-fs.sh
+++ RaspberryPi/raspberry-fs.sh
@@ -8,7 +8,7 @@ FILE="$SCRIPT_DIR/DietPi_RPi234-ARMv8-Bookworm.img"
 if [[ -f "$FILE" ]]; then
     echo "Found file: $FILE"
     echo chmod +x raspberry_f2fs.sh
-    echo sudo raspberry_f2fs.sh "$FILE" /dev/sdb
+    sudo $SCRIPT_DIR/raspberry_f2fs.sh "$FILE" /dev/sdb
 else
     echo "File not found!"
 fi

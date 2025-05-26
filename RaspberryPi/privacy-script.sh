3fc4aefef8f9ecedeb5186183f11def57b0c98e5
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-26 02:43:26 +0200

    Update privacy-script.sh

--- RaspberryPi/privacy-script.sh
+++ RaspberryPi/privacy-script.sh
@@ -10,7 +10,6 @@ if [ "$EUID" -ne 0 ]; then
 fi
 export HOME="/home/${SUDO_USER:-${USER}}" # Keep `~` and `$HOME` for user not `/root`.
 
-
 # ----------------------------------------------------------
 # --Disable Python history for future interactive commands--
 # ----------------------------------------------------------
@@ -23,6 +22,106 @@ fi
 sudo chattr +i "$(realpath $history_file)" # realpath in case of symlink
 # ----------------------------------------------------------
 
+# ----------------------------------------------------------
+# -------Disable participation in Popularity Contest--------
+# ----------------------------------------------------------
+echo '--- Disable participation in Popularity Contest'
+config_file='/etc/popularity-contest.conf'
+if [ -f "$config_file" ]; then
+  sudo sed -i '/PARTICIPATE/c\PARTICIPATE=no' "$config_file"
+else
+  echo "Skipping because configuration file at ($config_file) is not found. Is popcon installed?"
+fi
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# -------Remove Popularity Contest (`popcon`) package-------
+# ----------------------------------------------------------
+echo '--- Remove Popularity Contest (`popcon`) package'
+if ! command -v 'apt-get' &> /dev/null; then
+  echo 'Skipping because "apt-get" is not found.'
+else
+  apt_package_name='popularity-contest'
+if status="$(dpkg-query -W --showformat='${db:Status-Status}' "$apt_package_name" 2>&1)" \
+    && [ "$status" = installed ]; then
+  echo "\"$apt_package_name\" is installed and will be uninstalled."
+  sudo apt-get purge -y "$apt_package_name"
+else
+  echo "Skipping, no action needed, \"$apt_package_name\" is not installed."
+fi
+fi
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# -Remove daily cron entry for Popularity Contest (popcon)--
+# ----------------------------------------------------------
+echo '--- Remove daily cron entry for Popularity Contest (popcon)'
+job_name='popularity-contest'
+cronjob_path="/etc/cron.daily/$job_name"
+if [[ -f "$cronjob_path" ]]; then
+  if [[ -x "$cronjob_path" ]]; then
+    sudo chmod -x "$cronjob_path"
+    echo "Successfully disabled cronjob \"$job_name\"."
+  else
+    echo "Skipping, cronjob \"$job_name\" is already disabled."
+  fi
+else
+  echo "Skipping, \"$job_name\" cronjob is not found."
+fi
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# ----------------Remove `reportbug` package----------------
+# ----------------------------------------------------------
+echo '--- Remove `reportbug` package'
+if ! command -v 'apt-get' &> /dev/null; then
+  echo 'Skipping because "apt-get" is not found.'
+else
+  apt_package_name='reportbug'
+if status="$(dpkg-query -W --showformat='${db:Status-Status}' "$apt_package_name" 2>&1)" \
+    && [ "$status" = installed ]; then
+  echo "\"$apt_package_name\" is installed and will be uninstalled."
+  sudo apt-get purge -y "$apt_package_name"
+else
+  echo "Skipping, no action needed, \"$apt_package_name\" is not installed."
+fi
+fi
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# ----------Remove Python modules for `reportbug`-----------
+# ----------------------------------------------------------
+echo '--- Remove Python modules for `reportbug`'
+if ! command -v 'apt-get' &> /dev/null; then
+  echo 'Skipping because "apt-get" is not found.'
+else
+  apt_package_name='python3-reportbug'
+if status="$(dpkg-query -W --showformat='${db:Status-Status}' "$apt_package_name" 2>&1)" \
+    && [ "$status" = installed ]; then
+  echo "\"$apt_package_name\" is installed and will be uninstalled."
+  sudo apt-get purge -y "$apt_package_name"
+else
+  echo "Skipping, no action needed, \"$apt_package_name\" is not installed."
+fi
+fi
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# ----Remove UI for reportbug (`reportbug-gtk` package)-----
+# ----------------------------------------------------------
+echo '--- Remove UI for reportbug (`reportbug-gtk` package)'
+if ! command -v 'apt-get' &> /dev/null; then
+  echo 'Skipping because "apt-get" is not found.'
+else
+  apt_package_name='reportbug-gtk'
+if status="$(dpkg-query -W --showformat='${db:Status-Status}' "$apt_package_name" 2>&1)" \
+    && [ "$status" = installed ]; then
+  echo "\"$apt_package_name\" is installed and will be uninstalled."
+  sudo apt-get purge -y "$apt_package_name"
+else
+  echo "Skipping, no action needed, \"$apt_package_name\" is not installed."
+fi
+fi
 
 # ----------------------------------------------------------
 # -------------Clear system crash report files--------------
@@ -31,7 +130,6 @@ echo '--- Clear system crash report files'
 sudo rm -rfv /var/crash/*
 sudo rm -rfv /var/lib/systemd/coredump/
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # --------------Clear system logs (`journald`)--------------
@@ -46,14 +144,12 @@ sudo rm -rfv /run/log/journal/*
 sudo rm -rfv /var/log/journal/*
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # -----------Clear Zeitgeist data (activity logs)-----------
 # ----------------------------------------------------------
 echo '--- Clear Zeitgeist data (activity logs)'
 sudo rm -rfv {/root,/home/*}/.local/share/zeitgeist
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # ---------------Clear obsolete APT packages----------------
@@ -66,14 +162,12 @@ else
 fi
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # ---------------Clear APT package file lists---------------
 # ----------------------------------------------------------
 echo '--- Clear APT package file lists'
 sudo rm -rfv /var/lib/apt/lists/*
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # ---------Clear orphaned APT package dependencies----------
@@ -86,7 +180,6 @@ else
 fi
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # ---------------Clear cache for APT packages---------------
 # ----------------------------------------------------------
@@ -97,7 +190,6 @@ else
   sudo apt-get clean
 fi
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # -----------------Clear system-wide cache------------------
@@ -115,7 +207,6 @@ rm -rfv ~/.cache/*
 sudo rm -rfv root/.cache/*
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # --------------Clear thumbnails (icon cache)---------------
 # ----------------------------------------------------------
@@ -124,7 +215,6 @@ rm -rfv ~/.thumbnails/*
 rm -rfv ~/.cache/thumbnails/*
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # --------------Clear global temporary folders--------------
 # ----------------------------------------------------------
@@ -132,7 +222,6 @@ echo '--- Clear global temporary folders'
 sudo rm -rfv /tmp/*
 sudo rm -rfv /var/tmp/*
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # --------------------Clear screenshots---------------------
@@ -156,7 +245,6 @@ find ~ -name 'ksnip_*' | while read -r file_path; do
 done
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # -----------------------Empty trash------------------------
 # ----------------------------------------------------------
@@ -169,7 +257,6 @@ rm -rfv ~/snap/*/*/.local/share/Trash/*
 # Empty Flatpak trash (apps may not choose to use Portal API)
 rm -rfv ~/.var/app/*/data/Trash/*
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # ------------Clear GTK recently used files list------------
@@ -184,7 +271,6 @@ rm -fv ~/snap/*/*/.local/share/recently-used.xbel
 rm -fv ~/.var/app/*/data/recently-used.xbel
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # ------------Clear privacy.sexy script history-------------
 # ----------------------------------------------------------
@@ -193,7 +279,6 @@ echo '--- Clear privacy.sexy script history'
 glob_pattern="$HOME/.config/privacy.sexy/runs/*"
 rm -rfv $glob_pattern
 # ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 # -------------Clear privacy.sexy activity logs-------------
@@ -204,7 +289,6 @@ glob_pattern="$HOME/.config/privacy.sexy/logs/*"
 rm -rfv $glob_pattern
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # -------------------Clear Python history-------------------
 # ----------------------------------------------------------
@@ -213,7 +297,6 @@ rm -fv ~/.python_history
 sudo rm -fv /root/.python_history
 # ----------------------------------------------------------
 
-
 # ----------------------------------------------------------
 # --------------------Clear bash history--------------------
 # ----------------------------------------------------------
@@ -221,7 +304,6 @@ echo '--- Clear bash history'
 rm -fv ~/.bash_history
 sudo rm -fv /root/.bash_history
 # ----------------------------------------------------------
-
 
 echo 'Your privacy and security is now hardened 🎉💪'
 echo 'Press any key to exit.'

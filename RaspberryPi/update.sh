268a4c6a918e75aa922ad05ba4885ee07cce6f29
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-05-31 01:51:52 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get update --allow-releaseinfo-change
-sudo apt-get dist-upgrade -y
-sudo apt full-upgrade -y
-sudo dietpi-update
+sudo apt-get update --allow-releaseinfo-change && sudo apt-get dist-upgrade -y && sudo apt full-upgrade -y && sudo dietpi-update
+
 if command -v pihole > /dev/null; then
     sudo pihole -up
 else

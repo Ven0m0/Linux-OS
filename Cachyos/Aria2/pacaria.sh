445d41df09166a1a901790408296ba677eae1895
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-03 09:53:41 +0200

    Update pacaria.sh

--- Cachyos/Aria2/pacaria.sh
+++ Cachyos/Aria2/pacaria.sh
@@ -3,6 +3,8 @@
 URL="$1"
 OUTDIR="$2"
 OUTFILE="$3"
+cores="$(( ( $(nproc) / 2 ) - 1 ))"
+server="$(( ( $(nproc) / 4 ) - 1 ))"
 
 # Set CURL_BIN to curl-rustls if available, else curl
 if command -v curl-rustls &>/dev/null; then
@@ -18,5 +20,5 @@ if [[ "$URL" =~ \.(db|files|sig)(\.xz|\.zst|\.xz\.sig)?$ ]]; then
 else
     # Use aria2c for packages
     #aria2c -q -x 3 -s 7 -j 7 -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
-    aria2c -q -x $(($(nproc) - 1)) -s $(($(nproc) - 1)) -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
+    aria2c -q -x $server -s $cores -j $cores -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
 fi

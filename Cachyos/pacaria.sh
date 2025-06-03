65e8b3b4352e4608d977e2ad7f04d4bbca93f96d
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-03 09:41:00 +0200

    Update pacaria.sh

--- Cachyos/pacaria.sh
+++ Cachyos/pacaria.sh
@@ -17,5 +17,6 @@ if [[ "$URL" =~ \.(db|files|sig)(\.xz|\.zst|\.xz\.sig)?$ ]]; then
     "$CURL_BIN" -fL --retry 5 -o "${OUTDIR}/${OUTFILE}" "$URL"
 else
     # Use aria2c for packages
-    aria2c -q -x 3 -s 7 -j 7 -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
+    #aria2c -q -x 3 -s 7 -j 7 -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
+    aria2c -q -x $(($(nproc) - 1)) -s $(($(nproc) - 1)) -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
 fi

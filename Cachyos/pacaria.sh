a2399c9b1c622a53a6c15abbf6325149baade384
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-03 09:39:13 +0200

    Create pacaria.sh

---
+++ Cachyos/pacaria.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+URL="$1"
+OUTDIR="$2"
+OUTFILE="$3"
+
+# Set CURL_BIN to curl-rustls if available, else curl
+if command -v curl-rustls &>/dev/null; then
+    CURL_BIN="curl-rustls"
+else
+    CURL_BIN="curl"
+fi
+
+# Detect if URL is a sync database file
+if [[ "$URL" =~ \.(db|files|sig)(\.xz|\.zst|\.xz\.sig)?$ ]]; then
+    # Use selected curl for databases
+    "$CURL_BIN" -fL --retry 5 -o "${OUTDIR}/${OUTFILE}" "$URL"
+else
+    # Use aria2c for packages
+    aria2c -q -x 3 -s 7 -j 7 -R --optimize-concurrent-downloads=true -d "$OUTDIR" -o "$OUTFILE" "$URL"
+fi

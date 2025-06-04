7a09d04a8989bf6d52738aef078799454550d054
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-04 06:19:56 +0200

    Update alias.fish

--- Linux-Settings/Home/.config/fish/conf.d/alias.fish
+++ Linux-Settings/Home/.config/fish/conf.d/alias.fish
@@ -1,4 +1,4 @@
 alias ssh='dbclient'
 alias ptch='patch -p1 <'
-alias cleansh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | sudo bash'
-alias updatesh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Updates.sh | sudo bash'
+alias cleansh='curl-rustls -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | sudo bash'
+alias updatesh='curl-rustls -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Updates.sh | sudo bash'

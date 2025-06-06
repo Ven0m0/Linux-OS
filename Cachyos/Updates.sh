336687383b81e084b4e8b63f6bb47b14c2a8047a
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-06 13:33:25 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -7,7 +7,7 @@ sudo -v
 echo "🔄 Updating system..."
 sudo pacman -Syu --noconfirm
 sudo paru --cleanafter -Syu --devel --combinedupgrade
-sudo topgrade -c --disable config_update --skip-notify -y || true
+sudo topgrade -c --disable config_update --skip-notify -y --no-retry || true
 rustup update || true
 uv tool upgrade --all --compile-bytecode --native-tls
 tldr -u && sudo tldr -u

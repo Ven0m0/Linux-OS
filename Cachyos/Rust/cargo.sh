87a40f954faf846b85e56810128d758bb7d19fd0
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-06 18:27:45 +0200

    Update cargo.sh

--- Cachyos/Rust/cargo.sh
+++ Cachyos/Rust/cargo.sh
@@ -31,6 +31,29 @@ fn main() {
 }
 
 
+# parking_lot: More compact and efficient implementations of the standard synchronization primitives
+# https://crates.io/crates/parking_lot
+# wasm32-unknown-unknown --> only on nightly with "-C target-feature=+atomics"  in RUSTFLAGS and -Zbuild-std=panic_abort,std passed to cargo
+cargo add parking_lot
+#in cargo.toml:
+[dependencies]
+parking_lot = { version = "*", features = ["nightly", "deadlock_detection"] }
+parking_lot = { version = "*", features = ["nightly", "send_guard"] }
+# Note that the deadlock_detection and send_guard features are incompatible and cannot be used together.
+
+# Hashbrown: Google's high-performance SwissTable hash map
+# https://crates.io/crates/hashbrown
+cargo add hashbrown
+#in main.rs
+use hashbrown::HashMap;
+
+let mut map = HashMap::new();
+map.insert(1, "one");
+
+#in cargo.toml:
+hashbrown = { version = "*", features = ["nightly", "rayon", "inline-more"] }
+
+
 cargo install cargo-clean-all
 cargo clean-all -y
 

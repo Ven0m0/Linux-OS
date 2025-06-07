55fb96c9b14d74772c744cee6a16656f8c617a3e
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-07 23:06:43 +0200

    Update Apps.sh

--- Cachyos/Apps.sh
+++ Cachyos/Apps.sh
@@ -1,6 +1,6 @@
 
 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && sudo pacman-key --lsign-key 3056513887B78AEB 
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm && sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
 sudo nano /etc/pacman.conf

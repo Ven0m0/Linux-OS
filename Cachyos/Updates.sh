df086a63c7103a13909f01902ad329fc2da5ad61
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-07 07:54:59 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -10,6 +10,7 @@ sudo paru --cleanafter -Syu --devel --combinedupgrade || true
 sudo topgrade -c --disable config_update --skip-notify -y --no-retry --disable=uv || true
 uv tool upgrade --all --compile-bytecode --native-tls || true
 rustup update || true
+cargo-updater updater -u || true
 
 tldr -u && sudo tldr -u || true
 

24f2c9cb9bc29c7737d38e31909560367ea5d4a2
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-14 13:01:10 +0200

    Update alias.fish

--- Linux-Settings/Home/.config/fish/conf.d/alias.fish
+++ Linux-Settings/Home/.config/fish/conf.d/alias.fish
@@ -1,4 +1,25 @@
-alias ssh='dbclient'
+# alias ssh='dbclient'
 alias ptch='patch -p1 <'
-alias cleansh='curl-rustls -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | sudo bash'
-alias updatesh='curl-rustls -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Updates.sh | sudo bash'
+alias cleansh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Updates.sh | bash'
+alias updatesh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | bash'
+
+alias cat='bat --strip-ansi=auto'
+
+# Better sudo
+if type -q sudo-rs
+    alias sudo='sudo-rs'
+else if type -q doas
+    alias sudo='doas'
+end
+
+# Ripgrep
+if type -q rg
+    alias rg='rg --no-stats --color=auto'
+    alias grep='rg -uuu --no-stats --color=auto'
+    alias fgrep='rg -uuu --no-stats --color=auto -E UTF-8'
+    alias egrep='rg --no-stats --color=auto'
+else
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+end

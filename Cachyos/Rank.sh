00454fb2dba941418f76b453202e6c329230fb84
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-14 10:05:43 +0200

    Update Rank.sh

--- Cachyos/Rank.sh
+++ Cachyos/Rank.sh
@@ -2,13 +2,7 @@
 
 sudo -v
 
-# sudo pacman -S keyserver-rank-cachy --noconfirm
 sudo keyserver-rank --yes
-# sudo reflector -c Germany --sort rate --save /etc/pacman.d/mirrorlist
-# sudo reflector -c Germany --sort rate --save /etc/pacman.d/chaotic-mirrorlist
-# sudo reflector -c Germany --sort rate --save /etc/pacman.d/alhp-mirrorlist
-# sudo reflector -c Germany --sort rate --save /etc/pacman.d/cachyos-v3-mirrorlist
-# sudo reflector -c Germany --sort rate --save /etc/pacman.d/cachyos-mirrorlist
 sudo cachyos-rate-mirrors
 sudo pacman-db-upgrade
 echo "✔ Updated mirrorlists"

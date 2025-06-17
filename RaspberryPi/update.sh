d6118305188396a90c49cebd13c9425c81467a5f
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-17 15:37:54 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+sudo -v
+
 sudo apt-get update --allow-releaseinfo-change
 sudo apt-get dist-upgrade -y
 sudo apt full-upgrade -y
@@ -10,5 +12,7 @@ if command -v pihole > /dev/null; then
 else
     echo "Pi-hole is NOT installed"
 fi
+
+
 rpi-eeprom-update
 #sudo PRUNE_MODULES=1 rpi-update

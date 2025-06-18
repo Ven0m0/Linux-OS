26c21521b18f9ed950e604a23c7a02e69ea85f74
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-18 11:26:51 +0200

    Update ven0m0.fish

--- Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
+++ Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
@@ -1,14 +1,16 @@
 # https://github.com/iffse/pay-respects
 pay-respects fish --alias | source
 
-# alias ssh='dbclient'
+alias sshdb='dbclient'
 alias ptch='patch -p1 <'
 alias cleansh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Updates.sh | bash'
 alias updatesh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | bash'
 
 alias cat='bat --strip-ansi=auto'
-
 alias du='dust'
+
+alias cleanmem='sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'
+alias parallel='parallel -j 0 --load 75% --fast --pipe-part'
 
 # Better sudo
 if type -q sudo-rs

2ef74a3039e330461d6285c47376f3be5472447e
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-19 20:14:30 +0200

    Update build-opt.sh

--- Cachyos/Rust/build-opt.sh
+++ Cachyos/Rust/build-opt.sh
@@ -1,2 +1,6 @@
-export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -Z tune-cpu=native -Z default-visibility=hidden -Z gc -Z fmt-debug=none -Z location-detail=none -C link-arg=-fomit-frame-pointer -C link-arg=-fno-unwind-tables -C relro-level=off"
-export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z tune-cpu=native -Z default-visibility=hidden -Z gc -Z fmt-debug=none -Z location-detail=none -C link-arg=-fomit-frame-pointer -C link-arg=-fno-unwind-tables -C relro-level=off"
+#!/bin/bash
+
+export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto -Z tune-cpu=native \
+-Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C link-arg=-fomit-frame-pointer -C link-arg=-fno-unwind-tables -C relro-level=off"
+
+cargo +nightly build --release -Z unstable-options -Z gc -Z feature-unification -Z no-embed-metadata -Z avoid-dev-deps

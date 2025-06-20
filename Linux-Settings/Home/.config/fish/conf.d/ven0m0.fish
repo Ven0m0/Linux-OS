f7342c484d5da912dc8eb72bf09be6f9347d83d6
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-20 02:32:55 +0200

    Update ven0m0.fish

--- Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
+++ Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
@@ -8,10 +8,10 @@ alias cleansh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs
 alias updatesh='curl -fsSL https://raw.githubusercontent.com/Ven0m0/Linux-OS/refs/heads/main/Cachyos/Clean.sh | bash'
 
 alias cat='bat --strip-ansi=auto'
-alias du='dust'
+# alias du='dust -T 16 -b -P'
 
 alias cleanmem='sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'
-alias parallel='parallel -j 0 --load 75% --fast --pipe-part'
+# alias parallel='parallel -j 0 --load 75% --fast --pipe-part'
 
 # Better sudo
 if type -q sudo-rs

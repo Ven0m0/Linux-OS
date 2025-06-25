0ffd6c41233eca6483fd0dfcdcd259f9d78d0b03
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-25 11:34:23 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -5,7 +5,7 @@ sudo -v
 echo "🔄 Updating system..."
 sudo pacman -Syu --noconfirm -q 
 paru -Syu --noconfirm --combinedupgrade --nouseask -q --removemake --cleanafter --skipreview --nokeepsrc 
-topgrade -c --disable=config_update --skip-notify -y --no-retry --disable=uv --disable=pipx || true
+sudo -E topgrade -c --disable=config_update --skip-notify -y --no-retry --disable=uv --disable=pipx || true
 # pipx upgrade-all
 if command -v plasma-discover-update &>/dev/null; then
     export $(dbus-launch)

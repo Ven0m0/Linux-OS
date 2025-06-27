ec7f8796bc0fa5374bdad33f60880f1ccedf6e9e
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-27 06:09:21 +0200

    Update build-opt.sh

--- Cachyos/Rust/build-opt.sh
+++ Cachyos/Rust/build-opt.sh
@@ -40,7 +40,7 @@ cargo-cache -g -f -e clean-unref
 
 # Set optimization flags and build
 export RUSTFLAGS='-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto \
--Z tune-cpu=native -Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C link-arg=-fomit-frame-pointer -C link-arg=-fno-unwind-tables -C relro-level=off -Z threads=16'
+-Z tune-cpu=native -Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C relro-level=off'
 
 cargo +nightly build --release \
   -Z unstable-options \

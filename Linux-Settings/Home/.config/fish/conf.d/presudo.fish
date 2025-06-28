5e76e667fea42c280ddfdb7791bb924577168af4
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 00:48:38 +0200

    Update presudo.fish

--- Linux-Settings/Home/.config/fish/conf.d/presudo.fish
+++ Linux-Settings/Home/.config/fish/conf.d/presudo.fish
@@ -1,19 +1,26 @@
 function toggle_sudo
-  set line (commandline)
-  if test -z "$line"
-    set line (history | head -n1 | string trim)
-    if string match -r '^sudo ' -- "$line" > /dev/null
-      set line (string replace -r '^sudo\s+' '' -- "$line")
-    else
-      set line "sudo $line"
-    end
-  else if string match -r '^sudo ' -- "$line" > /dev/null
-    set line (string replace -r '^sudo\s+' '' -- "$line")
-  else
-    set line "sudo $line"
-  end
-  commandline --replace -- "$line"
-end
+    # Get current command line contents
+    set line (commandline)
 
-# Bind double Esc to toggle_sudo
-bind \e\e toggle_sudo
+    if test -z "$line"
+        # If command line empty, get last command from history and trim whitespace
+        set line (history | head -n1 | string trim)
+
+        # If it starts with sudo, remove it; else prepend sudo
+        if string match -r '^sudo ' -- "$line"
+            set line (string replace -r '^sudo\s+' '' -- "$line")
+        else
+            set line "sudo $line"
+        end
+    else
+        # If current command line starts with sudo, remove it; else prepend sudo
+        if string match -r '^sudo ' -- "$line"
+            set line (string replace -r '^sudo\s+' '' -- "$line")
+        else
+            set line "sudo $line"
+        end
+    end
+
+    # Replace current command line with modified command
+    commandline --replace -- "$line"
+end

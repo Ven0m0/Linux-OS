a8f06d72b0bb58098148740b50f5984d5b6272f3
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 00:53:39 +0200

    Update presudo.fish

--- Linux-Settings/Home/.config/fish/conf.d/presudo.fish
+++ Linux-Settings/Home/.config/fish/conf.d/presudo.fish
@@ -1,11 +1,9 @@
 function toggle_sudo
     # Get current command line contents
     set line (commandline)
-
     if test -z "$line"
         # If command line empty, get last command from history and trim whitespace
         set line (history | head -n1 | string trim)
-
         # If it starts with sudo, remove it; else prepend sudo
         if string match -r '^sudo ' -- "$line"
             set line (string replace -r '^sudo\s+' '' -- "$line")
@@ -20,28 +18,7 @@ function toggle_sudo
             set line "sudo $line"
         end
     end
-
     # Replace current command line with modified command
-    commandline --replace -- "$line"
-end
-
-
-function toggle_sudo
-    set line (commandline)
-    if test -z "$line"
-        set line (history | head -n1 | string trim)
-        if string match -r '^sudo ' -- "$line"
-            set line (string replace -r '^sudo\s+' '' -- "$line")
-        else
-            set line "sudo $line"
-        end
-    else
-        if string match -r '^sudo ' -- "$line"
-            set line (string replace -r '^sudo\s+' '' -- "$line")
-        else
-            set line "sudo $line"
-        end
-    end
     commandline --replace -- "$line"
 end
 

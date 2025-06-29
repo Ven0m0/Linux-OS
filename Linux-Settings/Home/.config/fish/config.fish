052b440f5c553685f7e235354dab0fac599b1e09
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 04:29:30 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -9,4 +9,3 @@ if test -d ~/.basher          ##basher5ea843
 end                           ##basher5ea843
 set -gx PATH $basher $PATH    ##basher5ea843
 status --is-interactive; and . (basher init - fish | psub)    ##basher5ea843
-source ~/.config/fish/conf.d/ven0m0.fish

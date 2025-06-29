3f02af4745f47778ea4524dbf20df75c33de4191
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 05:50:49 +0200

    Update env.fish

--- Linux-Settings/Home/.config/fish/conf.d/env.fish
+++ Linux-Settings/Home/.config/fish/conf.d/env.fish
@@ -1,22 +1,22 @@
 # Configuration environment variables
 
 # XDG
-set -g -x XDG_CONFIG_HOME "$HOME/.config"
-set -g -x XDG_CACHE_HOME "$HOME/.cache"
-set -g -x XDG_DATA_HOME "$HOME/.local/share"
-set -g -x XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
 
 # Qt 6
-set -g -x QT_QPA_PLATFORMTHEME qt6ct
-set -g -x QT_AUTO_SCREEN_SCALE_FACTOR 0
+set -gx QT_QPA_PLATFORMTHEME qt6ct
+set -gx QT_AUTO_SCREEN_SCALE_FACTOR 0
 
 # Fzf
-set -g -x FZF_LEGACY_KEYBINDINGS 0
+set -gx FZF_LEGACY_KEYBINDINGS 0
 
 # JetBrains IDE
-set -g -x _JAVA_AWT_WM_NONREPARENTING 1
-set -g -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
+set -gx _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true'
 
 # Firefox
-set -x MOZ_USE_XINPUT2 1
-set -x MOZ_ENABLE_WAYLAND 1
+set -gx MOZ_USE_XINPUT2 1
+set -gx MOZ_ENABLE_WAYLAND 1

279c0e81ac85537af709ce406c156f607e9c80b8
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 09:36:27 +0200

    Update cargopt.sh

--- Cachyos/Rust/cargopt.sh
+++ Cachyos/Rust/cargopt.sh
@@ -19,11 +19,11 @@ export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -Wno-error \
 	-fomit-frame-pointer -fvisibility=hidden -fmerge-all-constants -finline-functions \
 	-fbasic-block-sections=all -fjump-tables \
 	-pthread -falign-functions=32 -falign-loops=32 -malign-branch-boundary=32 -malign-branch=jcc \
-	-fshort-enums -fshort-wchar -feliminate-unused-debug-types -feliminate-unused-debug-symbols -fminimize-whitespace"
+	-fshort-enums -fshort-wchar -feliminate-unused-debug-types -feliminate-unused-debug-symbols"
  export CXXFLAGS="$CFLAGS -fsized-deallocation -fstrict-vtable-pointers -fno-rtti -fno-exceptions -Wp,-D_GLIBCXX_ASSERTIONS"
 LDFLAGS="-Wl,-O3 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now \
          -Wl,-z,pack-relative-relocs -Wl,-gc-sections \
-         -Wl,--discard-locals"
+         -Wl,--discard-locals -Wl,--strip-all"
 
 STRIP="llvm-strip -s --disable-deterministic-archives"
 STRIP="strip -s --disable-deterministic-archives"

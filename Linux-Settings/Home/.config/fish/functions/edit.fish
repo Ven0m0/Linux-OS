5a8dff3f9fb29eaec2bf8864f893a6177b618254
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 06:00:10 +0200

    Create edit.fish

---
+++ Linux-Settings/Home/.config/fish/functions/edit.fish
@@ -0,0 +1,7 @@
+function edit --wraps='$EDITOR' --description 'alias edit \$EDITOR'
+  $EDITOR $argv
+end
+
+function suedit --description 'Edit a file as root using \$EDITOR'
+    sudo $EDITOR $argv
+end

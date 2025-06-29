6455eed13a7920fd9bbc0d3f21af50f892dafab9
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 09:37:52 +0200

    Update cargopt.sh

--- Cachyos/Rust/cargopt.sh
+++ Cachyos/Rust/cargopt.sh
@@ -20,13 +20,12 @@ export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -Wno-error \
 	-fbasic-block-sections=all -fjump-tables \
 	-pthread -falign-functions=32 -falign-loops=32 -malign-branch-boundary=32 -malign-branch=jcc \
 	-fshort-enums -fshort-wchar -feliminate-unused-debug-types -feliminate-unused-debug-symbols"
- export CXXFLAGS="$CFLAGS -fsized-deallocation -fstrict-vtable-pointers -fno-rtti -fno-exceptions -Wp,-D_GLIBCXX_ASSERTIONS"
+export CXXFLAGS="$CFLAGS -fsized-deallocation -fstrict-vtable-pointers -fno-rtti -fno-exceptions -Wp,-D_GLIBCXX_ASSERTIONS"
 export LDFLAGS="-Wl,-O3 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now \
          -Wl,-z,pack-relative-relocs -Wl,-gc-sections -Wl,--compress-relocations \
          -Wl,--discard-locals -Wl,--strip-all -Wl,--icf=all"
-
-STRIP="llvm-strip -s --disable-deterministic-archives"
-STRIP="strip -s --disable-deterministic-archives"
+export STRIP="llvm-strip -s --disable-deterministic-archives"
+export STRIP="strip -s --disable-deterministic-archives"
 
 cargo +nightly build --release \
   -Z unstable-options \

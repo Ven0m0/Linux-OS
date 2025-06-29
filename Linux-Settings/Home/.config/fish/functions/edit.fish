d4509aeb2924ae2868302521ed12f7f038bb5e27
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-29 06:01:08 +0200

    Update edit.fish

--- Linux-Settings/Home/.config/fish/functions/edit.fish
+++ Linux-Settings/Home/.config/fish/functions/edit.fish
@@ -1,7 +1,7 @@
-function edit --wraps='$EDITOR' --description 'alias edit \$EDITOR'
+function edit --wraps='$EDITOR' --description 'alias edit $EDITOR'
   $EDITOR $argv
 end
 
-function suedit --description 'Edit a file as root using \$EDITOR'
+function suedit --description 'Edit a file as root using $EDITOR'
     sudo $EDITOR $argv
 end

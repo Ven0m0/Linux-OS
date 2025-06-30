c58467f084d34155c284a71493a9f180a1c5be92
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-06-30 14:34:38 +0200

    Update batmanurl.fish

--- Linux-Settings/Home/.config/fish/functions/batmanurl.fish
+++ Linux-Settings/Home/.config/fish/functions/batmanurl.fish
@@ -1,8 +1,12 @@
 function batmanurl
     if test (count $argv) -eq 0
-        echo "Usage: batmanurl <raw-github-url>"
+        echo "Usage: batmanurl <github-url>"
         return 1
     end
 
-    curl -s "$argv[1]" | bat -l man
+    # Extract raw URL from GitHub blob URL
+    set url $argv[1]
+    set raw_url (string replace -r 'https://github.com/([^/]+)/([^/]+)/blob/([^/]+)/(.*)' 'https://raw.githubusercontent.com/$1/$2/$3/$4' $url)
+
+    curl -s "$raw_url" | bat -l man
 end

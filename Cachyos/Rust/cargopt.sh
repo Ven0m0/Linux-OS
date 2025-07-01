31bfeed4de2eb5a390c3075d28b911f999d3bd40
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-01 13:40:54 +0200

    Update cargopt.sh

--- Cachyos/Rust/cargopt.sh
+++ Cachyos/Rust/cargopt.sh
@@ -11,13 +11,11 @@ export RUST_BACKTRACE="full"
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto -C relro-level=off -Z tune-cpu=native \
 -Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C debuginfo=0"
 export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -Wno-error \
-  -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -mharden-sls=none \
-  -fstack-clash-protection -fcf-protection=none -fno-semantic-interposition -fdata-sections -ffunction-sections \
+   	-mharden-sls=none -fcf-protection=none -fno-semantic-interposition -fdata-sections -ffunction-sections \
 	-mprefer-vector-width=256 -ftree-vectorize -fslp-vectorize \
 	-fomit-frame-pointer -fvisibility=hidden -fmerge-all-constants -finline-functions \
-	-fbasic-block-sections=all -fjump-tables \
-	-pthread -falign-functions=32 -falign-loops=32 -malign-branch-boundary=32 -malign-branch=jcc \
-	-fshort-enums -fshort-wchar -feliminate-unused-debug-types -feliminate-unused-debug-symbols"
+	-fbasic-block-sections=all -fjump-tables -fshort-enums -fshort-wchar \
+	-pthread -falign-functions=32 -falign-loops=32 -malign-branch-boundary=32 -malign-branch=jcc"
 export CXXFLAGS="$CFLAGS -fsized-deallocation -fstrict-vtable-pointers -fno-rtti -fno-exceptions -Wp,-D_GLIBCXX_ASSERTIONS"
 export LDFLAGS="-Wl,-O3 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now \
          -Wl,-z,pack-relative-relocs -Wl,-gc-sections -Wl,--compress-relocations \

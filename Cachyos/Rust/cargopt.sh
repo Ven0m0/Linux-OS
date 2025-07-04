207faf93c10dfb47179a8144ba9c29a5edac34c7
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-04 06:12:26 +0200

    Update cargopt.sh

--- Cachyos/Rust/cargopt.sh
+++ Cachyos/Rust/cargopt.sh
@@ -31,7 +31,9 @@ fi
 
 # Set optimization flags and build
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto -C relro-level=off -Z tune-cpu=native \
--Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C debuginfo=0"
+-Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none -C debuginfo=0 \
+-C prefer-inline "
+# -Z build-std=std,panic_abort
 export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -Wno-error \
    	-mharden-sls=none -fcf-protection=none -fno-semantic-interposition -fdata-sections -ffunction-sections \
 	-mprefer-vector-width=256 -ftree-vectorize -fslp-vectorize \

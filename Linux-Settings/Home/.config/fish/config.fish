d86e9ed323c2aa3c1e5edf533dac2e6bd2904091
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-04 14:29:27 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -6,7 +6,3 @@ end                           ##basher5ea843
 set -gx PATH $basher $PATH    ##basher5ea843
 status --is-interactive; and . (basher init - fish | psub)    ##basher5ea843
 
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME  $HOME/.cache
-set -x XDG_DATA_HOME   $HOME/.local/share
-set -x XDG_STATE_HOME  $HOME/.local/state

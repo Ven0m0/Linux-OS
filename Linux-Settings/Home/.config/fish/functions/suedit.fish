f33e1b911a03c4b8134f4bfea4f1a56d3ba80ff7
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-04 14:18:22 +0200

    Update suedit.fish

--- Linux-Settings/Home/.config/fish/functions/suedit.fish
+++ Linux-Settings/Home/.config/fish/functions/suedit.fish
@@ -1,3 +1,14 @@
 function suedit --description 'Edit a file as root using $EDITOR'
-    doas $EDITOR $argv
+    if test -z "$EDITOR"
+        echo "❌ \$EDITOR is not set. Please set your editor first." >&2
+        return 1
+    end
+
+    if type -q sudo-rs    
+        sudo-rs $EDITOR $argv[1]
+    else if type -q doas
+        doas $EDITOR $argv[1]
+    else
+        sudo $EDITOR $argv[1]
+    end
 end

74e0782fd66fe4de6b6e82111e2f28c59fe32acf
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-05 02:28:44 +0200

    Update build-opt.sh

--- Cachyos/Rust/build-opt.sh
+++ Cachyos/Rust/build-opt.sh
@@ -20,6 +20,8 @@ ZFLAGS="-Z unstable-options -Z gc -Z git -Z gitoxide -Z avoid-dev-deps -Z featur
 LTOFLAGS="-C lto=on -C embed-bitcode=yes -Z dylib-lto"
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C relro-level=off \
 	-Z tune-cpu=native -Z fmt-debug=none -Z location-detail=none -Z default-visibility=hidden ${LTOFLAGS} ${ZFLAGS}"
+# Parallel codegen frontend (no perf loss)
+export RUSTFLAGS="${RUSTFLAGS} -Z threads=16"
 # Research further
 CARGO_RESOLVER_INCOMPATIBLE_RUST_VERSIONS=fallback
 CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu

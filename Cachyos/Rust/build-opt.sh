57549b3cc0512b693285c4d3082a58431805907b
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-05 06:41:30 +0200

    Update build-opt.sh

--- Cachyos/Rust/build-opt.sh
+++ Cachyos/Rust/build-opt.sh
@@ -77,27 +77,13 @@ cargo-shear --fix
 cargo-machete --fix --with-metadata
 cargo-cache -g -f -e clean-unref
 
-# Set optimization flags and build 
-export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto -C relro-level=off -Z tune-cpu=native \
--Z default-visibility=hidden -Z fmt-debug=none -Z location-detail=none"
-# Append additional unstable flags
-export RUSTFLAGS="${RUSTFLAGS} -Z no-embed-metadata -Z trim-paths"
 # General flags
-export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -Wno-error \
-	-Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security -mharden-sls=none \
-	-fstack-clash-protection -fcf-protection=none -fno-semantic-interposition -fdata-sections -ffunction-sections \
-	-mprefer-vector-width=256 -ftree-vectorize -fslp-vectorize \
-	-fomit-frame-pointer -fvisibility=hidden -fmerge-all-constants -finline-functions \
-	-fbasic-block-sections=all -fjump-tables \
-	-pthread -falign-functions=32 -falign-loops=32 -malign-branch-boundary=32 -malign-branch=jcc \
-	-fshort-enums -fshort-wchar -feliminate-unused-debug-types -feliminate-unused-debug-symbols -std=c++23"
-export CXXFLAGS="${CFLAGS} -std=c++23 -fsized-deallocation -fstrict-vtable-pointers -fno-rtti -fno-exceptions -Wp,-D_GLIBCXX_ASSERTIONS"
-export LDFLAGS="-Wl,-O3 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now \
-	-Wl,-z,pack-relative-relocs -Wl,-gc-sections -Wl,--compress-relocations -Wl,--strip-unneeded \
-	-Wl,--discard-locals -Wl,--s -Wl,--icf=all-Wl,-U -Wl,--optimize-bb-jumps" 
-
 # Default build
-export NIGHTLYFLAGS="-Z unstable-options -Z gc -Z git -Z gitoxide -Z feature-unification -Z no-embed-metadata -Z avoid-dev-deps -Z trim-paths"
+export NIGHTLYFLAGS="-Z unstable-options -Z gc -Z git -Z gitoxide"
+export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C lto=on -C relro-level=off -C debuginfo=0 -C strip=symbols -C debuginfo=0 -C force-frame-pointers=no -C link-dead-code=no \
+-Z tune-cpu=native -Z default-visibility=hidden  -Z location-detail=none -Z function-sections"
+# -C embed-bitcode=yes -Z dylib-lto
+
 cargo +nightly ${NIGHTLYFLAGS} build --release
 
 ### Rustflags for pgo:

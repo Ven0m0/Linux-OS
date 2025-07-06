0cfba0f45e700a267d67c5a11b4e488466b85f50
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-06 19:08:43 +0200

    Update cargopt.sh

--- Cachyos/Rust/cargopt.sh
+++ Cachyos/Rust/cargopt.sh
@@ -38,6 +38,13 @@ if command -v sccache >/dev/null 2>&1; then
   export RUSTC_WRAPPER=sccache
 fi
 
+RCLANG="-C linker=clang -C link-arg=-fuse-ld=lld -C linker-plugin-lto"
+# Mold doesnt support "-C linker-plugin-lto" yet
+RMOLD="-C linker=clang -C link-arg=-fuse-ld=mold"
+
+export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C lto=on -C relro-level=off -C debuginfo=0 -C strip=symbols -C debuginfo=0 -C force-frame-pointers=no -C link-dead-code=no \
+-Z tune-cpu=native -Z default-visibility=hidden  -Z location-detail=none -Z function-sections ${RCLANG} "
+
 # Set optimization flags and build
 export ZFLAGS="-Z unstable-options -Z gc -Z git -Z gitoxide -Z avoid-dev-deps -Z no-embed-metadata -Z trim-paths -Z feature-unification"
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native -C codegen-units=1 -C strip=symbols -C lto=on -C embed-bitcode=yes -Z dylib-lto -C relro-level=off -Z tune-cpu=native \

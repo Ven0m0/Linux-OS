98acd8278ce11247be003a02c7f2284389778fc9
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-14 00:00:09 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -1,15 +1,18 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-# source ~/.config/fish/conf.d/ven0m0.fish
 
 if test -d ~/.basher
     set basher ~/.basher/bin
 end
 set -gx PATH $basher $PATH
 status --is-interactive; and . (basher init - fish | psub)
+
 # Prompt
 starship init fish | source
 pay-respects fish --alias | source
 zoxide init fish | source
+fzf --fish | source
 # Async prompt
-set -g async_prompt_functions fish_prompt fish_right_prompt
-set -g async_prompt_enable 1
+set -U async_prompt_functions fish_prompt fish_right_prompt
+set -gx async_prompt_enable 1
+# you see nothing stranger...
+set -gx SHELL_MOMMYS_ONLY_NEGATIVE true

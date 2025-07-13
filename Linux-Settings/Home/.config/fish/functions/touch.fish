a506ca46d396deaf12a3d9aa87bb496ec4c6d987
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-13 06:56:21 +0200

    Create touch.fish

---
+++ Linux-Settings/Home/.config/fish/functions/touch.fish
@@ -0,0 +1,14 @@
+function touch --description "touch + mkdir -p"
+  for f in $argv
+    set dir (dirname "$f")
+    set depth (count (string split '/' "$dir"))
+    if test "$dir" = "." -o "$dir" = ""
+      command touch "$f"
+    else if test $depth[1] -le 4
+      mkdir -p "$dir"; and command touch "$f"
+    else
+      echo "touch: directory for '$f' exceeds limit of 3 slashes" >&2
+      return 1
+    end
+  end
+end

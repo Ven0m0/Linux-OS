a66b5133b89085bb252ded86466c91802752c82a
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-13 05:56:22 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -1,9 +1,11 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+# source ~/.config/fish/conf.d/ven0m0.fish
 
 if test -d ~/.basher
     set basher ~/.basher/bin
 end
 set -gx PATH $basher $PATH
 status --is-interactive; and . (basher init - fish | psub)
+# Prompt
 starship init fish | source
 pay-respects fish --alias | source

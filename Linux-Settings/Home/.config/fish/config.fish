eccf0daa578958e8ca5a4d2e55276f1c98e31888
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-13 05:35:11 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -1,8 +1,8 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-if test -d ~/.basher          ##basher5ea843
-  set basher ~/.basher/bin    ##basher5ea843
-end                           ##basher5ea843
-set -gx PATH $basher $PATH    ##basher5ea843
-status --is-interactive; and . (basher init - fish | psub)    ##basher5ea843
-
+if test -d ~/.basher
+    set basher ~/.basher/bin
+end
+set -gx PATH $basher $PATH
+status --is-interactive; and . (basher init - fish | psub)
+starship init fish | source

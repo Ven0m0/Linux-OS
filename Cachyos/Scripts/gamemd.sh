76d74aeea2a50869b33c706ae54c3673dcc133ef
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-16 09:45:42 +0200

    Update gamemd.sh

--- Cachyos/Scripts/gamemd.sh
+++ Cachyos/Scripts/gamemd.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+LC_ALL=C
+LANG=C
+
 sudo -v
 
-
+echo kyber | sudo tee /sys/block/nvme0n1/queue/scheduler
+echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sudo powerprofilesctl set performance
 sudo cpupower frequency-set -g performance
+
+echo 1024 | sudo tee /sys/block/sda/queue/read_ahead_kb >/dev/null
+echo 1024 | sudo tee /sys/block/sda/queue/nr_requests >/dev/null
+echo 0 | sudo tee /sys/block/sda/queue/add_random >/dev/null

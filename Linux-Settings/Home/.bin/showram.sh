5dc3f2c67bcf94ae3ae1563bf66a83cac424ac72
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-20 06:11:07 +0200

    Create showram.sh

---
+++ Linux-Settings/Home/.bin/showram.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# shows the rum usage
+# part of shell-tools (GPLv3)
+
+ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' | sort

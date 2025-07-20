22035431d398012e32bf1e263e01d6a0931dd196
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-20 09:31:13 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -13,10 +13,24 @@ status --is-interactive; and . (basher init - fish | psub)
 #fzf --fish | source
 
 _evalcache starship init fish
-_evalcache batman --export-env
-_evalcache pay-respects fish --alias
-_evalcache zoxide init fish
+if type -q batman
+	_evalcache batman --export-env
+end
+if type -q zoxide
+	#zoxide init fish | source
+	_evalcache zoxide init fish
+end
+if type -q pay-respects
+	#pay-respects fish --alias | source
+	_evalcache pay-respects fish --alias
+end
+
 _evalcache fzf --fish
+
+#_evalcache starship init fish
+#_evalcache batman --export-env
+#_evalcache pay-respects fish --alias
+#_evalcache zoxide init fish
 
 # Async prompt
 set -U async_prompt_functions fish_prompt fish_right_prompt

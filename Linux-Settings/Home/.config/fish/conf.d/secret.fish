436f634c314ac861fe9dbb1849c595fcadef02d4
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-21 22:39:29 +0200

    Create secret.fish

---
+++ Linux-Settings/Home/.config/fish/conf.d/secret.fish
@@ -0,0 +1,7 @@
+# you see nothing...
+set -gx SHELL_MOMMYS_ONLY_NEGATIVE true
+set -gx MOMMY_SWEETIE girl
+alias margo='cargo mommy '
+alias gargo='cargo gg '
+set -gx CARGO_MOMMYS_MOODS chill/thirsty/yikes
+set -gx CARGO_MOMMYS_FUCKING pet/slut/toy/whore

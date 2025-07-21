f3842b5f99f6e50b0dd9d744bb4bd3021c1bb346
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-22 00:13:55 +0200

    Update ven0m0.fish

--- Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
+++ Linux-Settings/Home/.config/fish/conf.d/ven0m0.fish
@@ -3,12 +3,6 @@
   #crabfetch -d arch || fastfetch
 #end
 
-# ─── Paths─────────────────────────────────────────────────────────
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_STATE_HOME $HOME/.local/state
-
 # ─── Environment Tweaks ─────────────────────────────────────────────────────────
 set -gx EDITOR micro
 set -gx VISUAL $EDITOR

fc5e6c611038993d2c648ed32574932a75ddb584
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-23 01:50:52 +0200

    Update Setup.sh

--- RaspberryPi/Scripts/Setup.sh
+++ RaspberryPi/Scripts/Setup.sh
@@ -9,5 +9,9 @@ sudo dpkg-reconfigure dash
 
 #https://github.com/Botspot/pi-apps
 # App Store for FOSS Projects 
-echo "Install Pi-Apps"
-wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
+#echo "Install Pi-Apps"
+#wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
+
+echo "Install PiApps-terminal_bash-edition"
+wget -qO- https://raw.githubusercontent.com/Itai-Nelken/PiApps-terminal_bash-edition/main/install.sh | bash
+pi-apps update -y

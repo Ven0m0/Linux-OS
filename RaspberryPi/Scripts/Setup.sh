2e29704024d2fbdac515c4fdd223f6ebcd217533
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-23 01:43:27 +0200

    Update Setup.sh

--- RaspberryPi/Scripts/Setup.sh
+++ RaspberryPi/Scripts/Setup.sh
@@ -1,5 +1,8 @@
-echo Pi-Hole
+echo "Install Pi-Hole"
 curl -sSL https://install.pi-hole.net | sudo bash
 
-echo Replace Bash shell with Dash shell"
+echo "Install PiKISS <3"
+curl -sSL https://git.io/JfAPE | bash
+
+echo "Replace Bash shell with Dash shell"
 sudo dpkg-reconfigure dash

3a3718cb42caa6f18bb42dc44d2654a954ed0460
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-22 06:37:08 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -7,8 +7,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y 
 sudo apt full-upgrade -y
-#sudo dietpi-update
-sudo /boot/dietpi/dietpi-update
+sudo -E dietpi-update || sudo /boot/dietpi/dietpi-update
 
 if command -v pihole > /dev/null; then
     sudo pihole -up

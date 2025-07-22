59b9841f6ad8de14b1ebe326a5a26ea58ee9472d
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-23 01:53:50 +0200

    Update Setup.sh

--- RaspberryPi/Scripts/Setup.sh
+++ RaspberryPi/Scripts/Setup.sh
@@ -7,11 +7,20 @@ curl -sSL https://git.io/JfAPE | bash
 echo "Replace Bash shell with Dash shell"
 sudo dpkg-reconfigure dash
 
-#https://github.com/Botspot/pi-apps
+
 # App Store for FOSS Projects 
 #echo "Install Pi-Apps"
+#echo "#https://github.com/Botspot/pi-apps"
 #wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
 echo "Install PiApps-terminal_bash-edition"
+echo "https://github.com/Itai-Nelken/PiApps-terminal_bash-edition"
 wget -qO- https://raw.githubusercontent.com/Itai-Nelken/PiApps-terminal_bash-edition/main/install.sh | bash
 pi-apps update -y
+
+echo 
+echo "https://github.com/Drewsif/PiShrink"
+sudo apt update && sudo apt install -y wget parted gzip pigz xz-utils udev e2fsprogs
+wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
+chmod +x pishrink.sh
+sudo mv pishrink.sh /usr/local/bin

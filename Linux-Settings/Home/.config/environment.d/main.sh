467e6eb060337aaa68d8e067d947be031d6f2b77
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 11:48:49 +0200

    Create main.sh

---
+++ Linux-Settings/Home/.config/environment.d/main.sh
@@ -0,0 +1,23 @@
+# ~/.config/environment.d/*.sh
+
+EDITOR=micro
+VISUAL=micro
+GIT_EDITOR=micro
+
+LANGUAGE=en_US
+LANG=C.UTF-8
+#QT_QPA_PLATFORMTHEME=qt6ct
+
+MOZ_ENABLE_WAYLAND=1
+PROTON_ENABLE_WAYLAND=1
+ELECTRON_OZONE_PLATFORM_HINT=auto
+ELECTRON_ENABLE_LOGGING=false
+ELECTRON_ENABLE_STACK_DUMPING=false
+MIMALLOC_VERBOSE=0
+MIMALLOC_SHOW_ERRORS=0
+MIMALLOC_SHOW_STATS=0
+MIMALLOC_PURGE_DELAY=25
+MIMALLOC_ALLOW_LARGE_OS_PAGES=1
+ZSTD_NBTHREADS=0
+MALLOC_CONF="thp:always,metadata_thp:always,tcache:true,background_thread:true,percpu_arena:percpu"
+_RJEM_MALLOC_CONF="thp:always,metadata_thp:always,tcache:true,background_thread:true,percpu_arena:percpu"

331d2baaf1dee836ee8d29a55708b031c8b682d6
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 23:58:38 +0200

    Update Setup.sh

--- Cachyos/Setup.sh
+++ Cachyos/Setup.sh
@@ -1,4 +1,6 @@
-
+#!/usr/bin/env bash
+set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
+LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 
 
 sudo sed -i -e s"/\#LogFile.*/LogFile = /"g /etc/pacman.conf

a801ab2c7a3a8b6219edcf221c5aa2334e3e7ce3
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 23:57:20 +0200

    Update bleachbit.sh

--- Cachyos/Scripts/bleachbit.sh
+++ Cachyos/Scripts/bleachbit.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
+LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 
 # Download and install BleachBit custom cleaners
 REPO_URL="https://github.com/Ven0m0/Linux-OS.git"

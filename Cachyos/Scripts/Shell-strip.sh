d47194e51dedc1593fe38d661dfd6be9a2ee38bf
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 23:56:51 +0200

    Update Shell-strip.sh

--- Cachyos/Scripts/Shell-strip.sh
+++ Cachyos/Scripts/Shell-strip.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
-set -C
-IFS=$'\n\t'
-shopt -s nullglob globstar
+set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
+LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 
 usage() {
   cat <<EOF >&2

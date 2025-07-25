8aaf937bca1e7fa1158b38cf9b32115252cdf103
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 08:32:50 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -19,21 +19,19 @@ _evalcache starship init fish
 if type -q batman
 	_evalcache batman --export-env
 end
+if type -q batpipe
+	_evalcache batpipe
+end
 if type -q zoxide
-	#zoxide init fish | source
 	_evalcache zoxide init fish
 end
 if type -q pay-respects
 	#pay-respects fish --alias | source
 	_evalcache pay-respects fish --alias
 end
-
-_evalcache fzf --fish
-
-#_evalcache starship init fish
-#_evalcache batman --export-env
-#_evalcache pay-respects fish --alias
-#_evalcache zoxide init fish
+if type -q fzf
+	_evalcache fzf --fish
+fi
 
 # ─── Ghostty bash integration ─────────────────────────────────────────────────────────
 if test "$TERM" = "xterm-ghostty" -a -e "$GHOSTTY_RESOURCES_DIR"/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish

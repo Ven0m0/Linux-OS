a85e743d634b61e25ceaa50744a622c3bd164905
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 23:56:33 +0200

    Update Fix.sh

--- Cachyos/Scripts/Fix.sh
+++ Cachyos/Scripts/Fix.sh
@@ -1,4 +1,6 @@
-
-
+#!/usr/bin/env bash
+set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
+LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
+sudo -v
 sudo pacman-key --refresh-keys
 sudo pacman-key --populate

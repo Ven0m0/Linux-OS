2512ca42c2d10247fa3458b62633f43538069081
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-25 23:55:24 +0200

    Update and rename apk-optimizer.sh to android2-optimizer.sh

--- Cachyos/Scripts/Android/android2-optimizer.sh
+++ Cachyos/Scripts/Android/android2-optimizer.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-# shellcheck shell=bash
-set -euo pipefail
-IFS=$'\n\t'
-shopt -s nullglob globstar
-LC_ALL=C LANG=C
+set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
+LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 
 adb start-server
 adb shell setprop debug.enabletr 1

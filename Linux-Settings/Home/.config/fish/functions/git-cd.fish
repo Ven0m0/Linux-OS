5452e12cdf6fc33e2251455841802f09f4065128
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-27 20:20:51 +0200

    Update git-cd.fish

--- Linux-Settings/Home/.config/fish/functions/git-cd.fish
+++ Linux-Settings/Home/.config/fish/functions/git-cd.fish
@@ -1,21 +1,35 @@
-# Git clone and cd into the repo
-function git-cd
+function gitctl
     if test (count $argv) -eq 0
-        echo "Usage: git-cd <git-repo-url>"
+        echo "Usage: gitctl <git-repo-url> [directory]"
         return 1
     end
 
     set url $argv[1]
+    set dir ""
 
-    if type -q gix
-        gix clone $url
+    # Use provided directory name or derive from URL
+    if test (count $argv) -ge 2
+        set dir $argv[2]
     else
-        git clone $url
+        # Strip trailing slashes and .git suffix
+        set base (string replace --regex -- '(/|\.git)+$' '' $url)
+        set dir (basename $base)
+    end
+
+    # If directory already exists, just cd into it
+    if test -d $dir
+        cd $dir
+        return 0
+    end
+
+    # Clone with gix if available, otherwise git
+    if type -q gix
+        gix clone $url $dir
+    else
+        git clone $url $dir
     end
 
     or return 1  # Abort if clone failed
 
-    # Strip trailing slashes and .git suffix
-    set repo (basename (string replace --regex -- '(/|\.git)+$' '' $url))
-    cd $repo
+    cd $dir
 end

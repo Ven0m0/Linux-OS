080db814b180e552871f0ef974d5d3a2685e8541
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-28 02:59:15 +0200

    Update gamemd.sh

--- Cachyos/Scripts/gamemd.sh
+++ Cachyos/Scripts/gamemd.sh
@@ -21,11 +21,11 @@ echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governo
 sudo powerprofilesctl set performance
 sudo cpupower frequency-set -g performance
 
-echo 1024 | sudo tee /sys/block/sda/queue/read_ahead_kb >/dev/null
-echo 1024 | sudo tee /sys/block/sda/queue/nr_requests >/dev/null
+echo 512 | sudo tee /sys/block/nvme0n1/queue/nr_requests
+echo 1024 | sudo tee /sys/block/nvme0n1/queue/read_ahead_kb
 echo 0 | sudo tee /sys/block/sda/queue/add_random >/dev/null
 
-
+echo performance | sudo tee /sys/module/pcie_aspm/parameters/policy
 
 # disable bluetooth
 sudo systemctl stop bluetooth.service
@@ -36,10 +36,10 @@ for usb_device in /sys/bus/usb/devices/*/power/control; do
 done
 
 # disable NMI watchdog
-echo '0' | sudo tee /proc/sys/kernel/nmi_watchdog > /dev/null
+echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 
 # disable Wake-on-Timer
-echo '0' | sudo tee /sys/class/rtc/rtc0/wakealarm > /dev/null
+echo 0 | sudo tee /sys/class/rtc/rtc0/wakealarm
 
 export USE_CCACHE=1
 

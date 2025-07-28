adef849b9545df0eca18901ffe0405e74f09ab84
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-28 23:39:50 +0200

    Update template.sh

--- Cachyos/Scripts/shell-tools/template.sh
+++ Cachyos/Scripts/shell-tools/template.sh
@@ -6,6 +6,21 @@ shopt -s nullglob globstar
 # shopt -s extglob
 LC_COLLATE=C LC_CTYPE=C.UTF-8 LANG=C.UTF-8
 
+#----------------------------------------|
+# Color
+BLK='\e[30m' # Black
+RED='\e[31m' # Red
+GRN='\e[32m' # Green
+YLW='\e[33m' # Yellow
+BLU='\e[34m' # Blue
+MGN='\e[35m' # Magenta
+CYN='\e[36m' # Cyan
+WHT='\e[37m' # White
+# Effects
+DEF='\e[0m'   #Default color and effects
+BLD='\e[1m'   #Bold\brighter  
+#----------------------------------------|
+
 sudo -v
 
 WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"

c0d1bd87d364a82f137720a0e3324620c7da7b2c
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-28 23:20:55 +0200

    Create speedtest.sh

---
+++ Cachyos/Scripts/shell-tools/speedtest.sh
@@ -0,0 +1,10 @@
+
+
+# Download
+down=$(curl -s -o /dev/null -w "%{speed_download}" https://speed.cloudflare.com/__down?bytes=100000000)
+awk -v s="$down" 'BEGIN {printf "Download: %.2f Mbps\n", (s*8)/(1024*1024)}'
+
+# Upload (10 MB)
+up=$(dd if=/dev/zero bs=1M count=10 2>/dev/null | \
+  curl -s -o /dev/null -w "%{speed_upload}" --data-binary @- https://speed.cloudflare.com/__up)
+awk -v s="$up" 'BEGIN {printf "Upload: %.2f Mbps\n", (s*8)/(1024*1024)}'

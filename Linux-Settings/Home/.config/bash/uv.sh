c9d3b7507b61000d83603e60451644b19f6ccd2e
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-30 04:58:35 +0200

    Update uv.sh

--- Linux-Settings/Home/.config/bash/uv.sh
+++ Linux-Settings/Home/.config/bash/uv.sh
@@ -2,8 +2,10 @@
 
 # source ~/.config/bash/uv.sh
 
-export UV_COMPILE_BYTECODE=1
-export UV_NATIVE_TLS=1
-export UV_CONCURRENT_BUILDS=$(nproc)
-export UV_CONCURRENT_DOWNLOADS=$(( $(nproc) / 2 ))
-export UV_CONCURRENT_INSTALLS=$(nproc)
+if command -v uv &>/dev/null; then
+  export UV_COMPILE_BYTECODE=1
+  export UV_NATIVE_TLS=1
+  export UV_CONCURRENT_BUILDS=$(nproc)
+  export UV_CONCURRENT_DOWNLOADS=$(( $(nproc) / 2 ))
+  export UV_CONCURRENT_INSTALLS=$(nproc)
+fi

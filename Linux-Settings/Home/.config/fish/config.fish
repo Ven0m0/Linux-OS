1ad79e6940952244c4051e18183a89d32caf2a8c
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-07-31 14:46:28 +0200

    Update config.fish

--- Linux-Settings/Home/.config/fish/config.fish
+++ Linux-Settings/Home/.config/fish/config.fish
@@ -15,10 +15,8 @@ status --is-interactive; and . (basher init - fish | psub)
 bind --erase \cv
 
 # Prompt
-# _evalcache fzf --fish
-#_evalcache zoxide init fish
-
 _evalcache starship init fish
+
 if type -q batman
 	_evalcache batman --export-env
 end
@@ -40,3 +38,6 @@ bind \cs '__ethp_commandline_toggle_sudo.fish'
 # Async prompt
 set -U async_prompt_functions fish_prompt fish_right_prompt
 set -gx async_prompt_enable 1
+
+ _evalcache fzf --fish
+_evalcache zoxide init fish

c8106ceefdb028f4483358c88d41282e0ad1631d
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-03 03:11:10 +0200

    Update Clean.sh

--- Cachyos/Clean.sh
+++ Cachyos/Clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
 LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
-sync
-clear
+sync;clear
+
 banner=$(cat <<'EOF'
  ██████╗██╗     ███████╗ █████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗ 
 ██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║██║████╗  ██║██╔════╝ 

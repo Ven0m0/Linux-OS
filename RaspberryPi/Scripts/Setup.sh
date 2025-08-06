b0f9a657d23fad7cf18cb28b116a5e4f183df7ec
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-06 11:33:07 +0200

    Update Setup.sh

--- RaspberryPi/Scripts/Setup.sh
+++ RaspberryPi/Scripts/Setup.sh
@@ -36,6 +36,14 @@ sudo netselect-apt stable && sudo mv sources.list /etc/apt/sources.list && sudo 
 sudo sh -c 'echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/force-unsafe-io'
 
 
+# apt-fast
+sudo touch /etc/apt/sources.list.d/apt-fast.list && sudo micro /etc/apt/sources.list.d/apt-fast.list
+deb [signed-by=/etc/apt/keyrings/apt-fast.gpg] http://ppa.launchpad.net/apt-fast/stable/ubuntu focal main
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xBC5934FD3DEBD4DAEA544F791E2824A7F22B44BD" | sudo gpg --dearmor -o /etc/apt/keyrings/apt-fast.gpg
+sudo apt-get update && sudo apt-get install apt-fast
+
 ## Fix timeout for tty
 # Apply immediately
 sudo sysctl -w kernel.hung_task_timeout_secs=0

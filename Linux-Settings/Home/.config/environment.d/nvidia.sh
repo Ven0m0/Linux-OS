dbb6513b9059d111db889074976929d6e5627df8
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-06 08:09:50 +0200

    Update nvidia.sh

--- Linux-Settings/Home/.config/environment.d/nvidia.sh
+++ Linux-Settings/Home/.config/environment.d/nvidia.sh
@@ -1,2 +1,7 @@
 # increase Nvidia shader cache size to 12GB
 export __GL_SHADER_DISK_CACHE_SIZE=12000000000
+# https://github.com/AzimsTech/CHEF-KOCH-GamingTweaks/tree/master/Linux%20Gaming
+export __GL_SHADER_DISK_CACHE=1
+export __GL_THREADED_OPTIMIZATION=1
+export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+export mesa_glthread=true

eeac865f57f620a297ba62e21d802bd9a8ce7572
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-10 10:59:16 +0200

    Update index.sh

--- Cachyos/Scripts/Android/index.sh
+++ Cachyos/Scripts/Android/index.sh
@@ -1,8 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# Usage: ./touch_hidden_files.sh [path]
-# If no path given, uses current directory
 
-BASE_DIR="${1:-.}"
+BASE_DIR="/storage/emulated/0"
 
 find "$BASE_DIR" -type d -readable | while IFS= read -r dir; do
   for f in .metadata_never_index .noindex .trackerignore; do

3631291f536c113cfadfd42211c6988a84edad19
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-16 05:33:19 +0200

    Update Fix.sh

--- Cachyos/Scripts/Fix.sh
+++ Cachyos/Scripts/Fix.sh
@@ -4,9 +4,13 @@ LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 sudo -v
 
 # https://github.com/ekahPruthvi/cynageOS/blob/main/Scripts/error.sh
-# Run before installing cynageOS
 echo -e "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n██░▄▄▄██░▄▄▀██░▄▄▀██░▄▄▄░██░▄▄▀████░▄▄█░█████\n██░▄▄▄██░▀▀▄██░▀▀▄██░███░██░▀▀▄█▀▀█▄▄▀█░▄▄░██\n██░▀▀▀██░██░██░██░██░▀▀▀░██░██░█▄▄█▄▄▄█▄██▄██\n▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n"
 
+# SSH fix
+sudo chmod -R 700 ~/.ssh
+sudo chmod -R 700 ~/.gnupg
+
+# Other
 sudo pacman -Syu --noconfirm
 sudo pacman -S archlinux-keyring --noconfirm
 sudo pacman-key --refresh-keys
@@ -19,3 +23,4 @@ sudo pacman -Sy --needed base-devel
 
 echo "Fixing wlogout pgp keyring error"
 curl -sS https://keys.openpgp.org/vks/v1/by-fingerprint/F4FDB18A9937358364B276E9E25D679AF73C6D2F | gpg --import -
+

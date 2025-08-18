ed7fb73abbd579ba3107edf7cbdf3b319e4dd4b0
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-18 13:57:38 +0200

    Update Setup.sh

--- RaspberryPi/Scripts/Setup.sh
+++ RaspberryPi/Scripts/Setup.sh
@@ -61,8 +61,23 @@ sudo bash -c "$(curl -fsSL https://pacstall.dev/q/install || wget -q https://pac
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# Zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
+# Navi
+bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
 
+# Ripgrep-all
+# https://github.com/phiresky/ripgrep-all
+sudo apt install ripgrep pandoc poppler-utils ffmpeg
+
+sudo apt update && sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 ## Fix timeout for tty
 # Apply immediately

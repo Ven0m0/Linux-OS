d831a0ea12d051ef36c4ddb55abfa47868bca486
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-18 16:06:25 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -1,46 +1,31 @@
 #!/usr/bin/env bash
-#set -euo pipefail
-#IFS=$'\n\t'
-#shopt -s nullglob globstar
-#export LC_ALL="C" LANG="C.UTF-8"
-#WORKDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && pwd)"
-#cd $WORKDIR
-#──────────── Color & Effects ────────────
-BLK='\e[30m' # Black
-RED='\e[31m' # Red
-GRN='\e[32m' # Green
-YLW='\e[33m' # Yellow
-BLU='\e[34m' # Blue
-MGN='\e[35m' # Magenta
-CYN='\e[36m' # Cyan
-WHT='\e[37m' # White
-DEF='\e[0m'  # Reset to default
-BLD='\e[1m'  #Bold
+export LC_ALL=C LANG=C
+WORKDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && pwd)"
+cd $WORKDIR
 #──────────── Helpers ────────────────────
-# Check for command
 has() { command -v "$1" &>/dev/null; }
-# Print-echo
 p() { printf "%s\n" "$@"; }
-# Print-echo for color
-pe() { printf "%b\n" "$@"; }
-# Bash sleep replacement
-#sleepy() { read -rt "$1" <> <(:) &>/dev/null || :; }
 #─────────────────────────────────────────
-#sync; sleepy 1 || sleep 1
-#sudo -v
-if has apt-fast; then
-  apttool="apt-fast"
+sync; sudo -v
+if has nala; then
+  sudo nala fetch
+  sudo nala update -y
+  sudo nala upgrade -y
+  sudo nala clean
+  sudo nala autoclean
+  sudo nala autoremove
+elif has apt-fast; them
+  sudo apt-fast update -y
+  sudo apt-fast upgrade -y
+  sudo apt-fast dist-upgrade -y
+  sudo apt-fast full-upgrade -y
+  sudo apt-fast autoremove
 else
-  apttool="apt-get"
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt-get dist-upgrade -y 
+  sudo apt full-upgrade -y
 fi
-sudo "$apttool" update -y
-sudo "$apttool" upgrade -y
-sudo "$apttool" dist-upgrade -y 
-#sudo apt-get update -y
-#sudo apt-get upgrade -y
-#sudo apt-get dist-upgrade -y 
-sudo apt full-upgrade -y
-
 # Check's the broken packages and fix them
 sudo dpkg --configure -a
 if [ $? -ne 0 ]; then
@@ -54,6 +39,7 @@ if [ "$(type -t G_SUDO 2>/dev/null)" = function ]; then
 else
     sudo /boot/dietpi/dietpi-update
 fi
+sudo dietpi-update || sudo bash -c "dietpi-update"
 
 if has pihole; then
     sudo pihole -up
@@ -62,6 +48,6 @@ else
 fi
 
 has rpi-eeprom-update && sudo rpi-eeprom-update -a
-has rpi-update && sudo JUST_CHECK=1 rpi-update
-sudo JUST_CHECK=1 rpi-update
+has rpi-update && sudo PRUNE_MODULES=1 rpi-update
+#sudo JUST_CHECK=1 rpi-update
 # sudo PRUNE_MODULES=1 rpi-update

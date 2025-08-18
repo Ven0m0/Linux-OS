337c5dbc2eda48dd2016ed0b790c7000bed3445b
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-18 12:05:41 +0200

    Update raspberry-fs.sh

--- RaspberryPi/raspberry-fs.sh
+++ RaspberryPi/raspberry-fs.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-
-sudo -v
-
 WORKDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && pwd)"
+cd -- "$WORKDIR"
+# Default ISO: first matching DietPi image if $1 not provided
+find . -maxdepth 1 -type f \( -iname '*raspberry*.img' -o -iname '*dietpi*.img' \)
+ISO="${1:-$(find raspberryos.img/DietPi_RPi*.img 2>/dev/null | head -n1)}"
 
+# Default USB: first sdX device if $2 not provided
+USB="${2:-$(lsblk -dniA -o NAME | grep -E '^sd[a-z]' | head -n1 | sed 's|^|/dev/|')}}"
 
-
-FILE="${$1:-}"
-FILE="$WORKDIR/DietPi_RPi234-ARMv8-Bookworm.img"
-
-if [[ -f "$FILE" ]]; then
-    echo "Found file: $FILE"
-    echo chmod +x raspberry_f2fs.sh
-    sudo $SCRIPT_DIR/raspberry_f2fs.sh "$FILE" /dev/sdb
+if [[ -f $ISO ]]; then
+    echo "Found file: $ISO"
+    echo chmod +x "$WORKDIR/raspberry_f2fs.sh"
+    sudo "$WORKDIR/raspberry_f2fs.sh" "$ISO" "$USB"
 else
-    echo "File not found!"
+    echo "File not found!"; exit
 fi
 

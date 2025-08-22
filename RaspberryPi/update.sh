38f85c3e90b34437fed6ccad35dc6e19f0ec4696
Ven0m0 <82972344+Ven0m0@users.noreply.github.com>
2025-08-22 09:13:35 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -4,7 +4,7 @@ WORKDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && pwd)"
 cd $WORKDIR
 #──────────── Helpers ────────────────────
 has() { command -v "$1" &>/dev/null; }
-p() { printf "%s\n" "$@"; }
+xprintf() { printf "%s\n" "$@"; }
 #─────────────────────────────────────────
 sync; sudo -v
 if command -v nala &>/dev/null; then
@@ -22,17 +22,13 @@ fi
 # Check's the broken packages and fix them
 sudo dpkg --configure -a
 if [ $? -ne 0 ]; then
-    p "There were issues configuring packages."
+    xprintf "There were issues configuring packages."
 else
-    p "No broken packages found or fixed successfully."
+    xprintf "No broken packages found or fixed successfully."
 fi
 
-if [ "$(type -t G_SUDO 2>/dev/null)" = function ]; then
-    G_SUDO /boot/dietpi/dietpi-update 1
-else
-    sudo /boot/dietpi/dietpi-update
 fi
-sudo dietpi-update || sudo bash -c "dietpi-update"
+sudo dietpi-update 1 || sudo /boot/dietpi/dietpi-update 1
 
 if has pihole; then
     sudo pihole -up

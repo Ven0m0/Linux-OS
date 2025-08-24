9b7e5af3167a23bfa9a646daa1d64a3b0f4add8e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-08-24 14:35:09 +0200

    Update update.sh

--- RaspberryPi/update.sh
+++ RaspberryPi/update.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
-export LC_ALL=C LANG=C
+#──────────── Setup ────────────────────
+export LC_ALL=C LANG=C; shopt -s nullglob globstar
 WORKDIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && pwd)"
 cd $WORKDIR
 #──────────── Helpers ────────────────────
-has() { command -v "$1" &>/dev/null; }
-xprintf() { printf "%s\n" "$@"; }
-#─────────────────────────────────────────
-sync; sudo -v
-if command -v nala &>/dev/null; then
-  sudo nala fetch --auto --sources --fetches 5 --non-free -y -c DE
-  sudo nala upgrade
-  sudo nala autoremove && sudo nala autopurge
-elif command -v apt-fast &>/dev/null; then
-  sudo apt-fast update -y && sudo apt-fast upgrade -y
-  sudo apt-fast dist-upgrade -y && sudo apt-fast full-upgrade -y
-  sudo apt-fast autoremove
+has(){ command -v -- "$1" &>/dev/null; }
+hasname(){ local x=$(type -P -- "$1") || return; printf '%s\n' "${x##*/}"; }
+xprintf(){ printf "%s\n" "$@"; }
+#──────────── Sudo ────────────────────
+[[ -f /boot/dietpi/func/dietpi-globals ]] && . "/boot/dietpi/func/dietpi-globals" &>/dev/null || :
+suexec="$(hasname sudo-rs || hasname sudo || hasname doas)"
+[[ -z ${suexec:-} ]] && { echo "❌ No valid privilege escalation tool found (sudo-rs, sudo, doas)." >&2; exit 1; }
+[[ $EUID -ne 0 && $suexec =~ ^(sudo-rs|sudo)$ ]] && "$suexec" -v 2>/dev/null || :
+export HOME="/home/${SUDO_USER:-$USER}"; sync
+#─────────────────────────────────────────────────────────────
+if has nala; then
+  "$suexec" nala fetch --auto --sources --fetches 5 --non-free -y -c DE
+  "$suexec" nala upgrade
+  "$suexec" nala autoremove && "$suexec" nala autopurge
+elif has apt-fast; then
+  "$suexec" apt-fast update -y && "$suexec" apt-fast upgrade -y
+  "$suexec" apt-fast dist-upgrade -y && "$suexec" apt-fast full-upgrade -y
+  "$suexec" apt-fast autoremove
 else
-  sudo apt-get update -y --allow-releaseinfo-change && sudo apt-get upgrade -y
-  sudo apt-get dist-upgrade -y && sudo apt full-upgrade -y
+  "$suexec" apt-get update -y --allow-releaseinfo-change && "$suexec" apt-get upgrade -y
+  "$suexec" apt-get dist-upgrade -y && "$suexec" apt full-upgrade -y
 fi
 # Check's the broken packages and fix them
-sudo dpkg --configure -a
+"$suexec" dpkg --configure -a >/dev/null
 if [ $? -ne 0 ]; then
     xprintf "There were issues configuring packages."
 else
     xprintf "No broken packages found or fixed successfully."
 fi
 
-sudo dietpi-update 1 || sudo /boot/dietpi/dietpi-update 1
+"$suexec" dietpi-update 1 || "$suexec" /boot/dietpi/dietpi-update 1
 
-if has pihole; then
-    sudo pihole -up
-else
-    p "Pi-hole is NOT installed"
-fi
+has pihole && "$suexec" pihole -up || :
 
-has rpi-eeprom-update && sudo rpi-eeprom-update -a
-has rpi-update && sudo PRUNE_MODULES=1 rpi-update
-#sudo JUST_CHECK=1 rpi-update
-# sudo PRUNE_MODULES=1 rpi-update
+has rpi-eeprom-update && "$suexec" rpi-eeprom-update -a || :
+has rpi-update && "$suexec" PRUNE_MODULES=1 rpi-update || :
+#"$suexec" JUST_CHECK=1 rpi-update
+# "$suexec" PRUNE_MODULES=1 rpi-update

12af08e522c6ad2ed716aa4bbe7631e8201b4488
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-08-24 04:42:02 +0200

    Update Updates.sh

--- Cachyos/Updates.sh
+++ Cachyos/Updates.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export LC_ALL=C LANG=C; set -u
+shopt -s nullglob globstar
 #──────────── Color & Effects ────────────
 BLK=$'\e[30m' WHT=$'\e[37m' BWHT=$'\e[97m'
 RED=$'\e[31m' GRN=$'\e[32m' YLW=$'\e[33m'

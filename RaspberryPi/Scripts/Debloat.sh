0ec90f64060c97ba4aa649053ac8c8001190a0f7
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-08-24 13:18:48 +0200

    Update Debloat.sh

--- RaspberryPi/Scripts/Debloat.sh
+++ RaspberryPi/Scripts/Debloat.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-sudo apt-get remove --purge libreoffice* -y
-sudo apt-get autoremove -y
+sudo apt-get purge -y libreoffice*
+
+echo "Tweaks from https://privacy.sexy"
+sudo apt-get purge -y reportbug
+sudo apt-get purge -y python3-reportbug
+sudo apt-get purge -y reportbug-gtk
+sudo apt-get purge -y popularity-contest
+echo '--- Disable participation in Popularity Contest'
+if [[ -f /etc/popularity-contest.conf ]]; then
+  sudo sed -i '/^PARTICIPATE=/d;$aPARTICIPATE=no' "/etc/popularity-contest.conf"
+else
+  echo "Skipping: '/etc/popularity-contest.conf' not found"
+fi
+echo '--- Remove daily cron entry for Popularity Contest (popcon)'
+if [[ -f /etc/cron.daily/popularity-contest ]]; then
+  [[ -x /etc/cron.daily/popularity-contest ]] && sudo chmod -x "/etc/cron.daily/popularity-contest" && echo "Disabled cronjob." || echo "Already disabled."
+else
+  echo "Cronjob not found."
+fi
+
 sudo apt-get autoclean -y
+sudo apt-get autoremove -y

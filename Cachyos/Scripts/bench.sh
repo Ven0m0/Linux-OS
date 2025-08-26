b557b386522e926c124d4729736aa0dc52e8277f
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-08-26 23:14:51 +0200

    Update bench.sh

--- Cachyos/Scripts/bench.sh
+++ Cachyos/Scripts/bench.sh
@@ -4,25 +4,27 @@ export LC_ALL=C LANG=C
 
 #–– Helper to test for a binary in $PATH
 has() { command -v -- "$1" &>/dev/null; } # Check for command
-hasname(){ local x; x=$(type -P -- "$1") || return; printf '%s\n' "${x##*/}"; } # Get basename of command
-p() { printf '%s\n' "$@" 2>/dev/null; } # Print-echo
+hasname(){ local x=$(type -P -- "$1") && printf '%s\n' "${x##*/}"; }
+xecho() { printf '%s\n' "$@" 2>/dev/null; } # Print-echo
 suexec="$(hasname sudo-rs || hasname sudo || hasname doas)"
 [[ -z ${suexec:-} ]] && { p "❌ No valid privilege escalation tool found (sudo-rs, sudo, doas)." >&2; exit 1; }
-[[ $suexec =~ ^(sudo-rs|sudo)$ ]] && "$suexec" -v || :
-has hyperfine || { echo "❌ hyperfine not found in PATH"; exit 1; }
+[[ $EUID -ne 0 && $suexec =~ ^(sudo-rs|sudo)$ ]] && "$suexec" -v 2>/dev/null || :
+export HOME="/home/${SUDO_USER:-$USER}"
+has hyperfine || { xecho "❌ hyperfine not found in PATH"; exit 1; }
 
-o1="$(< /sys/devices/system/cpu/intel_pstate/no_turbo)"
-Reset() { 
-  "$suexec" sh -c 'echo "${o1:-0}" >/sys/devices/system/cpu/intel_pstate/no_turbo' &>/dev/null
+o1="$(</sys/devices/system/cpu/intel_pstate/no_turbo)"
+Reset(){ 
+  #echo "${o1:-0}" | sudo tee "/sys/devices/system/cpu/intel_pstate/no_turbo" &>/dev/null || :
+  echo 0 | "$suexec" tee "/sys/devices/system/cpu/intel_pstate/no_turbo" &>/dev/null || :
 }
 "$suexec" cpupower frequency-set --governor performance &>/dev/null || :
-"$suexec" sh -c 'echo 1 >/sys/devices/system/cpu/intel_pstate/no_turbo' &>/dev/null || :
+echo 1 | "$suexec" tee "/sys/devices/system/cpu/intel_pstate/no_turbo" &>/dev/null || :
 
 benchmark() {
   local name="$1"; shift
   local cmd="$*"
-  p "▶ $name"
-  hyperfine -w 25 -m 50 -i -S bash \
+  xprintf "▶ $name"
+  command hyperfine -w 25 -m 50 -i -S bash \
     -p "sync; echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null; systemd-resolve --flush-caches; hash -r" \
     "$cmd"
 }

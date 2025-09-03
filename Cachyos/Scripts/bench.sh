ba6d7cd0cfa5edbf94ef44c866af7a2b98c20258
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-03 14:30:18 +0200

    Update bench.sh

--- Cachyos/Scripts/bench.sh
+++ Cachyos/Scripts/bench.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-set -euo pipefail; shopt -s nullglob globstar
-export LC_ALL=C LANG=C
+export LC_ALL=C LANG=C; shopt -s nullglob
 
 #–– Helper to test for a binary in $PATH
 has() { command -v -- "$1" &>/dev/null; } # Check for command
 hasname(){ local x=$(type -P -- "$1") && printf '%s\n' "${x##*/}"; }
 xecho() { printf '%s\n' "$@" 2>/dev/null; } # Print-echo
 suexec="$(hasname sudo-rs || hasname sudo || hasname doas)"
-[[ -z ${suexec:-} ]] && { p "❌ No valid privilege escalation tool found (sudo-rs, sudo, doas)." >&2; exit 1; }
+[[ -z ${suexec:-} ]] && { echo "❌ No valid privilege escalation tool found (sudo-rs, sudo, doas)." >&2; exit 1; }
 [[ $EUID -ne 0 && $suexec =~ ^(sudo-rs|sudo)$ ]] && "$suexec" -v 2>/dev/null || :
 export HOME="/home/${SUDO_USER:-$USER}"
-has hyperfine || { xecho "❌ hyperfine not found in PATH"; exit 1; }
+has hyperfine || { echo "❌ hyperfine not found in PATH"; exit 1; }
 
 o1="$(</sys/devices/system/cpu/intel_pstate/no_turbo)"
 Reset(){ 
@@ -20,15 +19,14 @@ Reset(){
 "$suexec" cpupower frequency-set --governor performance &>/dev/null || :
 echo 1 | "$suexec" tee "/sys/devices/system/cpu/intel_pstate/no_turbo" &>/dev/null || :
 
-benchmark() {
+benchmark(){
   local name="$1"; shift
   local cmd="$*"
   xprintf "▶ $name"
   command hyperfine -w 25 -m 50 -i -S bash \
-    -p "sync; echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null; systemd-resolve --flush-caches; hash -r" \
+    -p "sync; echo 3 | doas tee /proc/sys/vm/drop_caches &>/dev/null; resolvectl flush-caches; hash -r" \
     "$cmd"
 }
-
 # Benchmarks
 benchmark "xargs" "seq 1000 | xargs -n1 -P$(nproc) echo"
 benchmark "parallel" "seq 1000 | parallel -j $(nproc) echo {}"

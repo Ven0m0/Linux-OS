1f827b5be15debd2230b7c16cce631e79ca52776
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-04 03:05:03 +0200

    Update aapt2.sh

--- Cachyos/Scripts/Android/aapt2.sh
+++ Cachyos/Scripts/Android/aapt2.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
-LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
+LC_ALL=C LANG=C
 
 # After your build step (where you already built the APK):
 APK_PATH="target/release/app-unsigned.apk"

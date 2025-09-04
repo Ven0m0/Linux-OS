404e76b8c5e9ce53677551d24c493de2504449b6
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-04 06:34:49 +0200

    Update uu-sym.sh

--- Cachyos/Scripts/WIP/uu-sym.sh
+++ Cachyos/Scripts/WIP/uu-sym.sh
@@ -3,13 +3,15 @@ export LC_ALL=C LANG=C
 # Non destructive uutils-coreutils symlink script. Links it to an arbitrary bin folder in HOME
 # Can easily be reverted unlike some other scripts that nuke your coreutils
 
-DEST="$HOME/bin/uutils"
-#DEST="$HOME/.local/bin"
+#DEST="$HOME/bin/uutils"
+DEST="$HOME/.local/bin"
 PKG="uutils-coreutils"
 SAFE_TOOLS=(
-  cat ls cp mv rm mkdir rmdir echo printf yes pwd dirname basename
-  touch stat du df head tail wc sort uniq cut tr fold split join
-  uname date id whoami groups true false test sleep
+cat ls cp mv rm mkdir rmdir echo printf printenv env yes pwd dirname basename
+touch stat dd du df head tail wc sort uniq cut tr fold split join
+uname date id whoami groups true false test [ sleep install more hostname
+chown chmod dircolors ln kill nice nohup nproc fmt numfmt seq realpath readlink
+users uptime tty who
 )
 mkdir -p "$DEST"
 pacman -Qlq "$PKG" | grep '/bin/uu-' |
@@ -18,12 +20,11 @@ while read -r path; do
   name=${bin#uu-}    # → "ls"
   for safe in "${SAFE_TOOLS[@]}"; do
     if [[ $name == "$safe" ]]; then
-      ln -sf "$path" "$DEST/$name"
+      ln -snf -- "$path" "${DEST}/${name}"
     fi
   done
 done
-export PATH="$HOME/bin/uutils:$PATH"
 echo "Symlinked safe uutils into $DEST"
-echo "  export PATH=\"${DEST}:\$PATH\""
+echo 'export PATH="${DEST}:${PATH}"'
 echo 'or'
-echo "  export PATH=\"\$HOME/bin/uutils:\$PATH\""
+echo 'export PATH="${HOME}/.local/bin:${PATH}"'

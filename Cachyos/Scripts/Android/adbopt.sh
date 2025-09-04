0a2514cb8d85e0bed912952f9e70763321cdb642
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-04 05:22:41 +0200

    Update adbopt.sh

--- Cachyos/Scripts/Android/adbopt.sh
+++ Cachyos/Scripts/Android/adbopt.sh
@@ -4,5 +4,39 @@ export LC_ALL=C LANG=C
 printf '%s\n' "Optimizing"
 
 adb shell cmd shortcut reset-all-throttling
-adb shell pm trim-caches 999999M
+adb shell pm trim-caches 128G
+adb shell pm art cleanup
 adb shell sm fstrim
+adb shell logcat -b all -c
+adb shell logcat -c
+adb shell sync
+adb shell wm density reset
+adb shell wm size reset
+#adb shell cmd package compile -a  -f --full --secondary-dex -m speed-profile
+adb shell cmd package compile -a  -f --full --secondary-dex -m speed
+adb shell pm art dexopt-packages -r bg-dexopt
+
+adb shell setprop debug.composition.type dyn
+adb shell setprop debug.performance.tuning 1
+adb shell setprop debug.fb.rgb565 0
+adb shell setprop debug.renderengine.backend skiavk
+adb shell setprop debug.hwui.renderer skiavk
+adb shell setprop debug.hwui.use_vulkan true
+adb shell setprop debug.sf.disable_threaded_present false
+adb shell setprop renderthread.skia.reduceopstasksplitting=true
+adb shell setprop debug.sf.predict_hwc_composition_strategy 1
+adb shell setprop debug.mdpcomp.enable 1
+adb shell logcat -G 128K -b main -b system
+adb shell logcat -G 64K -b radio -b events -b crash
+
+
+
+
+
+
+
+
+
+
+
+

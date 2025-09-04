9c614c4087ac8e962b47e5a5581683a21191b2ba
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-04 12:31:49 +0200

    Update copyparty.sh

--- RaspberryPi/Scripts/copyparty.sh
+++ RaspberryPi/Scripts/copyparty.sh
@@ -15,3 +15,10 @@ echo "copyparty:!::0:99999:7:::" >> /etc/shadow
 # Create home dir
 mkdir -p /var/lib/copyparty
 chown $uid:$gid /var/lib/copyparty
+
+# python3 /usr/local/bin/copyparty-en.py -e2dsa --ftp 3921 -z -i unix:777:/dev/shm/party.sock
+bg_run(){ nohup "$@" >/dev/null 2>&1 < /dev/null & disown; }
+bg_fullrun(){ nohup setsid "$@" >/dev/null 2>&1 < /dev/null & disown; }
+
+bg_run python3 /usr/local/bin/copyparty-en.py -e2dsa --ftp 3921 -z -i unix:777:/dev/shm/party.sock
+bg_fullrun python3 /usr/local/bin/copyparty-en.py -e2dsa --ftp 3921 -z -i unix:777:/dev/shm/party.sock

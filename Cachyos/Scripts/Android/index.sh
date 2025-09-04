a86838168f3c801f5e915dbdd9d7f64d5a31f091
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-04 02:42:22 +0200

    Update index.sh

--- Cachyos/Scripts/Android/index.sh
+++ Cachyos/Scripts/Android/index.sh
@@ -2,7 +2,7 @@
 
 BASE_DIR="/storage/emulated/0"
 
-find "$BASE_DIR" -type d -readable | while IFS= read -r dir; do
+find -O3 "$BASE_DIR" -type d -readable | while IFS= read -r dir; do
   for f in .metadata_never_index .noindex .trackerignore; do
     touch "$dir/$f"
   done

3c9aac7676fbe0578ae31260591d578b1b26b35e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-06 01:20:02 +0200

    Update Rank.sh

--- Cachyos/Rank.sh
+++ Cachyos/Rank.sh
@@ -3,6 +3,7 @@ set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
 LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
 sudo -v
 
+sudo pacman -Syy --noconfirm
 sudo keyserver-rank --yes
 sudo cachyos-rate-mirrors
 sudo pacman-db-upgrade

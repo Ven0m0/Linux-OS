52ba3111023861256e674f5f8929bca6aeb7b730
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-06 13:58:25 +0000

    Update profile-path.sh

--- Cachyos/Firefox/profile-path.sh
+++ Cachyos/Firefox/profile-path.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-firefox_profile() {
-  local prof base="$HOME/.mozilla/firefox" 
-  prof=$(awk -F= '
-    /^\[Install/{f=1; next}
-    /^\[/{f=0}
-    f && /^Default=/{print $2; exit}' "$base/profiles.ini")
-  [[ -n $prof ]] && echo "$base/$prof"
-}
+PROFILE_DIR="${HOME}/.mozilla/firefox"
+ACTIVE_PROF=$(awk -F= '/^Default=/ {print $2}' "${PROFILE_DIR}/installs.ini")
+#ACTIVE_PROF=$(awk -F= ' /^\[Install/{f=1; next} /^\[/{f=0} f && /^Default=/{print $2; exit}' "${PROFILE_DIR}/profiles.ini")
+
+echo "${PROFILE_DIR}/${ACTIVE_PROFILE}"
+PROFILE_DIR="${HOME}/.mozilla/firefox"
+[[ -n $AVTIVE_PROF ]] && APROF_DIR="$(echo "${PROFILE_DIR}/${ACTIVE_PROFILE}")"

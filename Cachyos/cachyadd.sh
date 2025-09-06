5117479f530c9d12043c550e26a6ffa92ab60aa2
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-06 08:36:00 +0000

    Create cachyadd.sh

---
+++ Cachyos/cachyadd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Add cachyos repo's to your existing arch install automatically
+# https://wiki.cachyos.org/features/optimized_repos/
+curl https://mirror.cachyos.org/cachyos-repo.tar.xz -O && { \
+  tar xvf cachyos-repo.tar.xz && \
+  cd cachyos-repo; \
+  chmod +x cachyos-repo.sh \
+  && sudo ./cachyos-repo.sh; \
+  }

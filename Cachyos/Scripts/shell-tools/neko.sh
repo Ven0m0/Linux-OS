6480cb4f1bce5776a2f8f55661be3bbf389ae3e2
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-06 14:36:14 +0000

    Update neko.sh

--- Cachyos/Scripts/shell-tools/neko.sh
+++ Cachyos/Scripts/shell-tools/neko.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-CATEGORY="${1:-neko}"  # default to 'neko' if no argument
-
-IMG_URL=$(curl -s "https://nekos.best/api/v2/$CATEGORY" | jaq -r '.results[0].url')
-curl -s "$IMG_URL" | chafa -O 6 -w 4 --clear
+nekofetch(){
+  local CAT="${1:-}" JT="${2:-}" IT="${3:-}"
+  if [[ -n "$JT" ]] && command -v "$JT" &>/dev/null; then
+    JSONT="$JT"
+  elif command -v jaq &>/dev/null; then
+    JSONT=jaq
+  elif command -v jq &>/dev/null; then
+    JSONT=jq
+  fi
+  if [[ -n $IMGT ]] && command -v "$IMGT" &>/dev/null; then
+    IMGT="$IT"
+  elif command -v chafa &>/dev/null; then
+    IMGT=chafa
+  fi
+  if [[ -z $CAT ]]; then
+  IMG_URL="$(curl -s "https://nekos.best/api/v2/${CAT}" | "$JSONT" -r '.results[0].url')"
+  curl -s "$IMG_URL" | "$IMGT" -O 6 -w 4 --clear
+  printf '%s\n' "Usage: nekofetch <category> <json-tool>"
+  printf '%s\n' "  categories: husbando, kitsune, neko, waifu"
+  printf '%s\n' "  JSON tools: jaq, jq"
+}

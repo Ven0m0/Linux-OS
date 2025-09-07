1907b34ead106935edd37ed65215eb05dd26c53b
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-07 02:28:36 +0200

    Update Shell-strip2.sh

--- Cachyos/Scripts/Shell-strip2.sh
+++ Cachyos/Scripts/Shell-strip2.sh
@@ -1,34 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
-LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
-set -x
-
+LC_ALL LANG=C.UTF-8
 INPUT="${1:?Usage: $0 <script.sh>}"
 
-# Strips comments from a Bash source file.
-pp_strip_comments() {
-	sed '/^[[:space:]]*#.*$/d'
-}
+pp_strip_comments()  { sed '/^[[:space:]]*#.*$/d'; }
+pp_strip_copyright() { awk '/^#/ {if(!p){ next }} { p=1; print }'; }
+pp_strip_separators(){ awk '/^#\s*-{5,}/ { next } { print }'; }
 
-# Strips copyright comments from the start of a Bash source file.
-pp_strip_copyright() {
-	awk '/^#/ {if(!p){ next }} { p=1; print $0 }'
-}
-
-# Strips separator comments from the start of a Bash source file.
-pp_strip_separators() {
-	awk '/^#\s*-{5,}/ { next; } {print $0}'
-}
-
-# Process the script
-cat "$INPUT" \
-  | pp_strip_separators \
+{
+  pp_strip_separators <"$INPUT" \
   | pp_strip_copyright \
   | pp_strip_comments \
   | shfmt -mn -ln bash -fn -ci -kp \
-  | shellharden \
-  > "${INPUT%.sh}.optimized.sh"
+  | shellharden
+} >"${INPUT%.sh}.optimized.sh"
 
-# Lint the result
 shellcheck "${INPUT%.sh}.optimized.sh"
 echo "Optimized script saved as ${INPUT%.sh}.optimized.sh"

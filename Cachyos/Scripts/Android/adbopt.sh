3e8bec8e9899207a4d45d5d518b181e32cf45abb
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-08 17:35:36 +0200

    Update adbopt.sh

--- Cachyos/Scripts/Android/adbopt.sh
+++ Cachyos/Scripts/Android/adbopt.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env bash
 
+# https://github.com/vaginessa/adb-cheatsheet
+
+
 printf '%s\n' "Setup"
 export LC_ALL=C LANG=C
 
-pacman -S android-tools
+pacman -S 
+if ! command -v adb &>/dev/null; then
+  if command -v pacman &>/dev/null; then
+    pacman -Qq android-tools &>/dev/null || sudo pacman -Sq --noconfirm --noprogressbar --needed android-tools
+  elif command -v apt-get; then
+if ; then
+  echo "installed"
+else
+  echo "not installed"
+fi
+
+pkg_installed(){
+  local pkg=$1
+  if command -v pacman &>/dev/null; then
+    pacman -Qq "$pkg" &>/dev/null
+  elif command -v dpkg-query &>/dev/null; then
+    dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "ok installed"
+  else
+    return 2
+  fi
+}
+
 
 
 adb start-server
@@ -11,6 +35,8 @@ adb devices
 #adb get-state
 
 printf '%s\n' "Cleanup"
+adb shell sync
+adb shell cmd stats write-to-disk
 adb shell pm art cleanup
 adb shell pm trim-caches 128G
 adb shell cmd shortcut reset-all-throttling
@@ -19,7 +45,6 @@ adb shell logcat -c
 adb shell wm density reset
 adb shell wm size reset
 adb shell sm fstrim # prob root only
-adb shell sync
 
 printf '%s\n' "Optimizing ART"
 # Does it twice to force speed-profile for all and does only speed for apps that might benefit without overwriting
@@ -46,6 +71,77 @@ adb shell logcat -G 64K -b radio -b events -b crash
 printf '%s\n' "Performance"
 adb shell setprop debug.performance.tuning 1
 adb shell setprop debug.mdpcomp.enable 1
+
+
+printf '%s\n' "Other"
+adb shell cmd uimode night yes 
+adb shell cmd uimode car no
+adb shell cmd -w wifi force-country-code enabled DE
+adb shell cmd -w wifi force-low-latency-mode enabled
+adb shell cmd wifi force-low-latency-mode enabled
+adb shell cmd -w wifi force-hi-perf-mode enabled
+adb shell cmd wifi force-hi-perf-mode enabled
+# Sets the interval between RSSI polls to milliseconds.
+#adb shell cmd -w wifi set-poll-rssi-interval-msecs <int>
+
+#pm list packages
+# List disabled packages
+pm list packages -d
+# Filter to only show enabled packages
+pm list packages -e
+# Filter to only show third party packages
+pm list packages -3
+# Set the default home activity (aka launcher)
+#adb shell cmd package set-home-activity [--user USER_ID] TARGET-COMPONENT
+
+#Print all applications in use
+adb shell pm list packages | sed -e "s/package://" | \
+  while read x; do adb shell cmd package resolve-activity --brief $x | tail -n 1 | grep -v "No activity found"; done
+
+# List all active services
+#adb shell dumpsys -l
+# Older devices
+#adb shell dumpsys -l |sed 's/^  /      /g'
+
+# Print codecs for bluetooth headphones
+adb shell dumpsys media.audio_flinger | grep -A3 Input 
+
+# Dump Settings
+adb shell dumpsys settings
+
+# Erase old stats for battery:
+adb shell dumpsys batterystats --reset 
+
+# Sort Applications By Ram Usage:
+adb shell dumpsys meminfo
+
+# Open Special Menu
+# am start -a android.intent.action.VIEW \
+# Open settings:
+ am start -n com.android.settings/com.android.settings.Settings
+# Start prefered webbrowser:
+am start -a android.intent.action.VIEW -d <url> (com.android.browser | com.android.chrome | com.sec.android.sbrowser)
+# Open any URL in default browser
+am start -a android.intent.action.VIEW -d <url>
+# Print Activities:
+am start -a com.android.settings/.wifi.CaptivePortalWebViewActivity
+
+# Auto rotation off
+content insert –uri content://settings/system –bind name:s:accelerometer_rotation –bind value:i:0
+# Rotate portrait
+content insert –uri content://settings/system –bind name:s:user_rotation –bind value:i:0
+
+# Adopting USB-Drive
+sm set-force-adoptable true
+
+# Print data in .db files, clean:
+grep -vx -f <(sqlite3 Main.db .dump) <(sqlite3 ${DB} .schema) 
+# Use below command fr update dg.db file:
+sqlite3 /data/data/com.google.android.gms/databases/dg.db "update main set c='0' where a like '%attest%';" 
+
+
+
+
 
 
 

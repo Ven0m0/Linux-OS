af7654a867a3a6b07f755ddf376618c87cfd2d50
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-08 16:20:59 +0200

    Update adbopt.sh

--- Cachyos/Scripts/Android/adbopt.sh
+++ Cachyos/Scripts/Android/adbopt.sh
@@ -1,42 +1,51 @@
 #!/usr/bin/env bash
+
+printf '%s\n' "Setup"
 export LC_ALL=C LANG=C
 
-printf '%s\n' "Optimizing"
+pacman -S android-tools
 
-adb shell cmd shortcut reset-all-throttling
-adb shell pm trim-caches 128G
+
+adb start-server
+adb devices
+#adb get-state
+
+printf '%s\n' "Cleanup"
 adb shell pm art cleanup
-adb shell sm fstrim
+adb shell pm trim-caches 128G
+adb shell cmd shortcut reset-all-throttling
 adb shell logcat -b all -c
 adb shell logcat -c
-adb shell sync
 adb shell wm density reset
 adb shell wm size reset
-#adb shell cmd package compile -a  -f --full --secondary-dex -m speed-profile
+adb shell sm fstrim # prob root only
+adb shell sync
+
+printf '%s\n' "Optimizing ART"
+# Does it twice to force speed-profile for all and does only speed for apps that might benefit without overwriting
+adb shell cmd package compile -af --full --secondary-dex -m speed-profile
 adb shell cmd package compile -a  -f --full --secondary-dex -m speed
 adb shell pm art dexopt-packages -r bg-dexopt
 
+printf '%s\n' "General rendering"
 adb shell setprop debug.composition.type dyn
-adb shell setprop debug.performance.tuning 1
 adb shell setprop debug.fb.rgb565 0
-adb shell setprop debug.renderengine.backend skiavk
-adb shell setprop debug.hwui.renderer skiavk
-adb shell setprop debug.hwui.use_vulkan true
 adb shell setprop debug.sf.disable_threaded_present false
-adb shell setprop renderthread.skia.reduceopstasksplitting=true
+adb shell setprop renderthread.skia.reduceopstasksplitting true
 adb shell setprop debug.sf.predict_hwc_composition_strategy 1
-adb shell setprop debug.mdpcomp.enable 1
+
+printf '%s\n' "Vulkan"
+adb shell setprop debug.renderengine.backend skiavk
+adb shell setprop debug.hwui.renderer skiavk # or 'skiagl'
+adb shell setprop debug.hwui.use_vulkan true
+
+printf '%s\n' "Logs"
 adb shell logcat -G 128K -b main -b system
 adb shell logcat -G 64K -b radio -b events -b crash
 
-
-
-
-
-
-
-
-
+printf '%s\n' "Performance"
+adb shell setprop debug.performance.tuning 1
+adb shell setprop debug.mdpcomp.enable 1
 
 
 

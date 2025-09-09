e0076d7736488c51fa14858ac56a36d72080ed5e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-09 22:32:54 +0200

    Update Shell-strip2.sh

--- Cachyos/Scripts/Shell-strip2.sh
+++ Cachyos/Scripts/Shell-strip2.sh
@@ -2,8 +2,8 @@
 LC_ALL LANG=C.UTF-8
 INPUT="${1:?Usage: $0 <script.sh>}"
 
-pp_strip_comments()  { sed '/^[[:space:]]*#.*$/d'; }
-pp_strip_copyright() { awk '/^#/ {if(!p){ next }} { p=1; print }'; }
+pp_strip_comments(){ sed '/^[[:space:]]*#.*$/d'; }
+pp_strip_copyright(){ awk '/^#/ {if(!p){ next }} { p=1; print }'; }
 pp_strip_separators(){ awk '/^#\s*-{5,}/ { next } { print }'; }
 
 {
@@ -16,3 +16,5 @@ pp_strip_separators(){ awk '/^#\s*-{5,}/ { next } { print }'; }
 
 shellcheck "${INPUT%.sh}.optimized.sh"
 echo "Optimized script saved as ${INPUT%.sh}.optimized.sh"
+
+

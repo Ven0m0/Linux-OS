af479a722ea21edf3337789597c272df1fb09c59
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-11 20:42:13 +0200

    Update Image-compress.sh

--- Cachyos/Scripts/WIP/Image-compress.sh
+++ Cachyos/Scripts/WIP/Image-compress.sh
@@ -4,4 +4,6 @@
 
 # https://github.com/ravgeetdhillon/create-optimize-images
 # https://github.com/VirtuBox/img-optimize
-
+# https://github.com/jonathongrigg/Android-Bootanimation-Optimizer
+# https://github.com/JafarAkhondali/Tapnesh
+# 

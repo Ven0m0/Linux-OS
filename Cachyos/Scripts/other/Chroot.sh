7f2c882f485f8758cee605ce73bc919b152474f5
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-12 17:01:47 +0200

    Create Chroot.sh

---
+++ Cachyos/Scripts/other/Chroot.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+LC_ALL=C
+sync; sudo -v
+
+lsblk -A
+sudo mount /dev/nvme0n1p2 /mnt
+sudo mount --bind /dev /mnt/dev 
+sudo mount --bind /proc /mnt/proc
+sudo mount --bind /sys /mnt/sys
+sudo mount --bind /run /mnt/run
+
+sudo chroot /mnt
+
+micro /etc/fstab
+
+sudo umount /mnt/run
+sudo umount /mnt/sys
+sudo umount /mnt/proc
+sudo umount /mnt/dev
+sudo umount /mnt
+
+sudo reboot
+
+
+
+# mkinitcpio -P

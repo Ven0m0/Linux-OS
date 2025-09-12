593ae2f151489d9582f2904590e281a8b8574dfc
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-12 17:23:56 +0200

    Update Chroot.sh

--- Cachyos/Scripts/other/Chroot.sh
+++ Cachyos/Scripts/other/Chroot.sh
@@ -4,21 +4,29 @@ sync; sudo -v
 
 lsblk -A
 sudo mount /dev/nvme0n1p2 /mnt
-sudo mount --bind /dev /mnt/dev 
-sudo mount --bind /proc /mnt/proc
-sudo mount --bind /sys /mnt/sys
-sudo mount --bind /run /mnt/run
+sudo mount /dev/nvme0n1p1 /mnt/boot
+#sudo mount /dev/nvme0n1p1 /mnt/boot/efi
 
-sudo chroot /mnt
+sudo mount -t proc proc /mnt/proc
+sudo mount --rbind /sys /mnt/sys
+sudo mount --rbind /dev /mnt/dev
+sudo mount --rbind /run /mnt/run
 
-micro /etc/fstab
+sudo arch-chroot /mnt /bin/bash
 
+echo "micro /etc/fstab"
+echo "micro /etc/mkinitcpio.conf"
+
+
+sudo mkinitcpio -P
 exit
-sudo umount /mnt/run
-sudo umount /mnt/sys
-sudo umount /mnt/proc
-sudo umount /mnt/dev
-sudo umount /mnt
+sudo umount -R /mnt/run
+sudo umount -R /mnt/dev
+sudo umount -R /mnt/sys
+sudo umount -R /mnt/proc
+sudo umount /mnt/boot/efi
+# sudo umount /mnt/boot 
+umount /mnt
 
 sudo reboot
 

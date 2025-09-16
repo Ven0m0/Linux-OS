5b90550cfae438b7ddc4b94ea5d833fe289c4b45
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-16 16:45:02 +0200

    Update adbopt.sh

--- Cachyos/Scripts/Android/adbopt.sh
+++ Cachyos/Scripts/Android/adbopt.sh
@@ -28,11 +28,8 @@ pkg_installed(){
   fi
 }
 
-
-
 adb start-server
 adb devices
-#adb get-state
 
 printf '%s\n' "Cleanup"
 adb shell sync
@@ -46,36 +43,104 @@ adb shell wm density reset
 adb shell wm size reset
 adb shell sm fstrim # prob root only
 
-printf '%s\n' "Optimizing ART"
+printf '%s\n' "Optimizing ART..."
+# Run any postponed dex‐opt jobs immediately 
+adb shell cmd jobscheduler run -f android \
+  $(adb shell cmd jobscheduler list-jobs android \
+  | grep background-dexopt | awk '{print $2}')
+
 # Does it twice to force speed-profile for all and does only speed for apps that might benefit without overwriting
 adb shell cmd package compile -af --full --secondary-dex -m speed-profile
 adb shell cmd package compile -a  -f --full --secondary-dex -m speed
 adb shell pm art dexopt-packages -r bg-dexopt
 
-printf '%s\n' "General rendering"
+
+printf '%s\n' "General rendering tweaks..."
 adb shell setprop debug.composition.type dyn
 adb shell setprop debug.fb.rgb565 0
 adb shell setprop debug.sf.disable_threaded_present false
 adb shell setprop renderthread.skia.reduceopstasksplitting true
 adb shell setprop debug.sf.predict_hwc_composition_strategy 1
-adb shell settings set global force_gpu_rendering 1
+adb shell setprop debug.hwui.use_buffer_age true
 adb shell setprop debug.hwui.render_dirty_regions true
+adb shell settings put global force_gpu_rendering 1
+adb shell settings put global debug.hwui.force_gpu_command_drawing 1
+adb shell settings put global debug.hwui.use_disable_overdraw 1
 
-printf '%s\n' "Vulkan"
-adb shell setprop debug.renderengine.backend skiavk
-adb shell setprop debug.hwui.renderer skiavk # or 'skiagl'
-adb shell setprop debug.hwui.use_vulkan true
+printf '%s\n' "Configuring Vulkan..."
+vk_set(){
+  adb shell setprop debug.renderengine.backend skiavk
+  adb shell setprop debug.hwui.renderer skiavk
+  adb shell setprop debug.hwui.use_vulkan true
+}
+gl_set(){
+  adb shell setprop debug.renderengine.backend skiaglthreaded
+  adb shell setprop debug.hwui.renderer skiagl
+  adb shell setprop debug.hwui.use_vulkan false
+}
 
-printf '%s\n' "Logs"
+printf '%s\n' "Configuring Webview..."
+echo "webview --enable-features=DeferImplInvalidation,ScrollUpdateOptimizations" > /data/local/tmp/webview-command-line
+adb shell chmod 644 /data/local/tmp/webview-command-line
+adb shell cmd webviewupdate set-webview-implementation com.android.webview.beta
+
+printf '%s\n' "Configuring ANGLE..."
+angle_on(){
+  adb shell settings put global angle_gl_driver_all_angle 1
+  adb shell settings put global angle_debug_package com.android.angle
+  adb shell settings put global angle_gl_driver_selection_values angle
+  adb shell settings put global angle_gl_driver_selection_pkgs com.android.webview,com.android.webview.beta
+}
+angle_off(){
+  adb shell settings delete global angle_debug_package
+  adb shell settings delete global angle_gl_driver_all_angle
+  adb shell settings delete global angle_gl_driver_selection_values
+  adb shell settings delete global angle_gl_driver_selection_pkgs
+}
+angle_on
+
+printf '%s\n' "Logs..."
 adb shell logcat -G 128K -b main -b system
 adb shell logcat -G 64K -b radio -b events -b crash
 
-printf '%s\n' "Performance"
+printf '%s\n' "Performance tweaks..."
 adb shell setprop debug.performance.tuning 1
 adb shell setprop debug.mdpcomp.enable 1
 
+printf '%s\n' "Battery tweaks..."
+adb shell cmd power suppress-ambient-display true
+adb shell cmd power set-face-down-detector false
+# adb shell cmd power set-mode 1/0
+adb shell cmd power set-fixed-performance-mode-enabled false
+# adb shell cmd power set-fixed-performance-mode-enabled true/false
+adb shell cmd power set-adaptive-power-saver-enabled true
+# adb shell cmd power set-adaptive-power-saver-enabled true/false
+adb shell settings put system accelerometer_rotation 0
 
 printf '%s\n' "Other"
+adb shell settings put global gpu_rasterization_forced 1
+adb shell settings put global enable_lcd_text 1
+adb shell setprop debug.aw.power_scheduler_enable_idle_throttle 1
+adb shell setprop debug.aw.cpu_affinity_little 1
+adb shell setprop debug.sf.disable_backpressure 0
+adb shell setprop debug.debuggerd.disable 1
+adb shell setprop debug.sf.enable_hwc_vds 1
+adb shell setprop debug.tracing.mnc 0
+adb shell setprop debug.tracing.battery_status 0
+adb shell setprop debug.tracing.screen_state 0
+adb shell cmd system_update
+adb shell cmd otadexopt cleanup
+
+# Improve scroll responsiveness apparently
+gfx_set(){ adb shell cmd gfxinfo "$1" reset && adb shell cmd gfxinfo "$1" framestats; }
+
+
+
+# Aggressive AppStandby / Doze toggles
+adb shell cmd deviceidle force-idle
+adb shell cmd deviceidle unforce
+doze_app(){ adb shell cmd deviceidle whitelist +"$1"; }
+
 adb shell cmd uimode night yes 
 adb shell cmd uimode car no
 adb shell cmd -w wifi force-country-code enabled DE
@@ -85,14 +150,17 @@ adb shell cmd -w wifi force-hi-perf-mode enabled
 adb shell cmd wifi force-hi-perf-mode enabled
 # Sets the interval between RSSI polls to milliseconds.
 #adb shell cmd -w wifi set-poll-rssi-interval-msecs <int>
+adb shell cmd wifi set-scan-always-available disabled
+
+
 
 #pm list packages
 # List disabled packages
-pm list packages -d
+adb shell pm list packages -d
 # Filter to only show enabled packages
-pm list packages -e
+adb shell pm list packages -e
 # Filter to only show third party packages
-pm list packages -3
+adb shell pm list packages -3
 # Set the default home activity (aka launcher)
 #adb shell cmd package set-home-activity [--user USER_ID] TARGET-COMPONENT
 
@@ -118,30 +186,30 @@ adb shell dumpsys batterystats --reset
 adb shell dumpsys meminfo
 
 # Open Special Menu
-# am start -a android.intent.action.VIEW \
+# adb shell am start -a android.intent.action.VIEW \
 # Open settings:
- am start -n com.android.settings/com.android.settings.Settings
+adb shell am start -n com.android.settings/com.android.settings.Settings
 # Start prefered webbrowser:
-am start -a android.intent.action.VIEW -d <url> (com.android.browser | com.android.chrome | com.sec.android.sbrowser)
+adb shell am start -a android.intent.action.VIEW -d <url> (com.android.browser | com.android.chrome | com.sec.android.sbrowser)
 # Open any URL in default browser
-am start -a android.intent.action.VIEW -d <url>
+adb shell am start -a android.intent.action.VIEW -d <url>
 # Print Activities:
-am start -a com.android.settings/.wifi.CaptivePortalWebViewActivity
+adb shell am start -a com.android.settings/.wifi.CaptivePortalWebViewActivity
 
 # Auto rotation off
-content insert –uri content://settings/system –bind name:s:accelerometer_rotation –bind value:i:0
+adb shell content insert –uri content://settings/system –bind name:s:accelerometer_rotation –bind value:i:0
 # Rotate portrait
-content insert –uri content://settings/system –bind name:s:user_rotation –bind value:i:0
+adb shell content insert –uri content://settings/system –bind name:s:user_rotation –bind value:i:0
 
 # Adopting USB-Drive
-sm set-force-adoptable true
+adb shell sm set-force-adoptable true
 
 # Print data in .db files, clean:
 grep -vx -f <(sqlite3 Main.db .dump) <(sqlite3 ${DB} .schema) 
 # Use below command fr update dg.db file:
 sqlite3 /data/data/com.google.android.gms/databases/dg.db "update main set c='0' where a like '%attest%';" 
 
-
+WSS_set(){ adb shellpm grant "$1" android.permission.WRITE_SECURE_SETTINGS }
 
 
 

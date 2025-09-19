586ca7dc41a4f2ed8d28c34ed67c3237f0108aa6
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-19 11:04:14 +0200

    Update Fix.sh

--- Cachyos/Scripts/Fix.sh
+++ Cachyos/Scripts/Fix.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
 set -euo pipefail; LC_ALL=C LANG=C.UTF-8
-
-# https://github.com/ekahPruthvi/cynageOS/blob/main/Scripts/error.sh
-echo -e "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n██░▄▄▄██░▄▄▀██░▄▄▀██░▄▄▄░██░▄▄▀████░▄▄█░█████\n██░▄▄▄██░▀▀▄██░▀▀▄██░███░██░▀▀▄█▀▀█▄▄▀█░▄▄░██\n██░▀▀▀██░██░██░██░██░▀▀▀░██░██░█▄▄█▄▄▄█▄██▄██\n▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n"
-
-sudo -v
-sudo pacman -Syu --noconfirm
-command -v paru &>/dev/null && paru -Syu --noconfirm
-command -v yay &>/dev/null && yay -Syu --noconfirm
+# https://github.com/ekahPruthvi/cynageOS
 # https://wiki.cachyos.org/cachyos_basic/faq/
-echo "Fix mirrors"
-command -v cachyos-rate-mirrors &>/dev/null && sudo cachyos-rate-mirrors
-sudo rm -r /var/cache/pacman/pkg/*
+SHELL="${BASH:-$(command -vp bash)}"
+export HOME="/home/${SUDO_USER:-$USER}"
+sudo -v; sync
+builtin cd -P -- "${BASH_SOURCE[0]%/*}"
+cd "$(printf '%s\n' ${BASH_SOURCE[0]%/*})"
+WORKDIR="$(builtin cd -Pe -- ${BASH_SOURCE[0]%/*} && builtin printf '%s\n' "${PWD:-$(pwd -P)}")"
+builtin cd -- "$WORKDIR" || exit 1
+cd -- "$(builtin cd -Pe -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && builtin printf '%s\n' ${PWD:-$(pwd)})"
+
+builtin cd -Pe -- "$(command dirname -- "${BASH_SOURCE[0]:-}" && builtin printf '%s\n' "${PWD:-$(pwd -P)}")" || exit 1
+
+WORKDIR="$(builtin cd -- "$(dirname -- "${BASH_SOURCE[0]:-}")" && printf '%s\n' "$PWD")"
+builtin cd -- "$WORKDIR" || exit 1
+
+update(){
+  [[ -f /var/lib/pacman/db.lck ]] && sudo rm -f --preserve-root -- /var/lib/pacman/db.lck >/dev/null
+  sudo pacman -Syu --noconfirm
+  command -v paru &>/dev/null && { paru -Sua --noconfirm; paru -Syu --noconfirm; }
+  command -v yay &>/dev/null && { yay -Sua --noconfirm; yay -Syu --noconfirm; }
+}
+
+mirrorfix(){
+  echo "Fix mirrors"
+  command -v cachyos-rate-mirrors &>/dev/null && sudo cachyos-rate-mirrors
+}
+
+cache(){
+  sudo rm -r /var/cache/pacman/pkg/*
+  sudo pacman -Sccq --noconfirm
+  command -v paru &>/dev/null && paru -Sccq --noconfirm
+  command -v yay &>/dev/null && yay -Sccq --noconfirm
+}
+
 
 # SSH fix
 sudo chmod -R 744 ~/.ssh; sudo chmod -R 744 ~/.gnupg

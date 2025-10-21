6beca6d7b105b9aeba119759ef983b8474bde2df
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-21 15:40:34 +0200

    Update Chaotic-aur.sh

--- Cachyos/Scripts/Chaotic-aur.sh
+++ Cachyos/Scripts/Chaotic-aur.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail; IFS=$'\n\t'; shopt -s nullglob globstar
-LC_COLLATE=C LC_CTYPE=C LANG=C.UTF-8
+LC_ALL=C
 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
+conf=/etc/pacman.conf
+repo='[chaotic-aur]'
+mirror='Include = /etc/pacman.d/chaotic-mirrorlist'
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+has_repo(){ command grep -qF "$repo" "$conf"; }
 
-echo "add this to /etc/pacman.conf"
-echo "[chaotic-aur]"
-echo "Include = /etc/pacman.d/chaotic-mirrorlist"
-
-echo "'sudo pacman -Syu' afterwards"
+if ! has_repo; then
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' \
+               'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+  {
+    echo
+    echo "$repo"
+    echo "$mirror"
+  } | sudo tee -a "$conf" >/dev/null
+  echo "chaotic-aur repo added. run: sudo pacman -Syu"
+  sudo pacman -Syyu --noconfirm
+else
+  echo "chaotic-aur repo already present"
+fi

9f02d65631ca9615c27fff70daf9b5c39e754a1e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-29 14:46:37 +0100

    Refactor package installation commands in rustify.sh
    Updated package installation commands to include --needed and --noconfirm flags for better handling of dependencies and confirmations.

--- Cachyos/Rust/rustify.sh
+++ Cachyos/Rust/rustify.sh
@@ -5,27 +5,27 @@ cd -P -- "$(cd -P -- "${BASH_SOURCE[0]%/*}" && echo "$PWD")" || exit 1
 sudo -v
 
 # Gnu utils
-sudo pacman -S uutils-coreutils
+sudo pacman --needed --noconfirm -S uutils-coreutils
 paru -S uutils-diffutils uutils-findutils uutils-procps-git 
 # Sed
 paru -S uutils-sed-git
-sudo pacman -S sd
+sudo pacman --needed --noconfirm -S sd
 # Tar
-sudo pacman -Rns tar && paru -S uutils-tar-git
+sudo pacman --needed --noconfirm -Rns tar && paru -S uutils-tar-git
 # Du
-sudo pacman -S dust
+sudo pacman --needed --noconfirm -S dust
 # Touch + mkdir
 cargo install touchp
 # Faster cp & rm
 cargo install cpz rmz
 # Grep
-sudo pacman -S ripgrep ripgrep-all repgrep
+sudo pacman --needed --noconfirm -S ripgrep ripgrep-all repgrep
 # Bash
-sudo pacman -S brush
+sudo pacman --needed --noconfirm -S brush
 # Find
-sudo pacman -S fd
+sudo pacman --needed --noconfirm -S fd
 # Cut
-sudo pacman -S hck
+sudo pacman --needed --noconfirm -S hck
 # Uniq
 cargo install runiq
 # Minify
@@ -33,12 +33,20 @@ cargo install minhtml
 # Wget
 cargo install kelpsget
 # Update-alternatives for arch
-sudo pacman -S zenity --noconfirm --needed
+sudo pacman --needed --noconfirm -S zenity --noconfirm --needed
 cargo install --git "https://github.com/fthomys/update-alternatives"
 pbin="$(command -v update-alternatives || echo ${HOME}/.cargo/bin/update-alternatives)"
 sudo ln -sf "$pbin" "/usr/local/bin/$(basename $pbin)"
 # oxidizr-arch (switch to uutils, sudo-rs)
-paru -S oxidizr-arch
+paru --skipreview --needed --noconfirm -S oxidizr-arch
+echo -e "run: 
+paru --skipreview --needed --noconfirm -S uutils-coreutils uutils-findutils-bin sudo-rs
+sudo oxidizr-arch --commit use coreutils
+sudo oxidizr-arch --commit use findutils
+sudo oxidizr-arch --commit use sudo"
 # dnsmasq / systemdresolved
 curl -sf "https://raw.githubusercontent.com/Ven0m0/Linux-OS/main/Cachyos/Rust/etchdns.sh" | bash
+# JQ
+sudo pacman --needed --noconfirm -S jaq
 #
+

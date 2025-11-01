558f9449e42b6081d165516464ccb8f46296cccb
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-01 06:07:19 +0100

    Update go-optimize.sh

--- Cachyos/Scripts/other/go-optimize.sh
+++ Cachyos/Scripts/other/go-optimize.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 LC_ALL=C
 
+export CGO_ENABLED=0 GOGC=200 GOMAXPROCS="$(nproc)" GOFLAGS="-ldflags=-s -w -trimpath -modcacherw -pgo auto"
+go telemetry off
 go install github.com/dkorunic/betteralign/cmd/betteralign@latest
 go install github.com/johnsiilver/goptimizer@latest
 betteralign -apply -fix -generated_files ./...
-goptimizer --goflags="--ldflags=-s -w"
+goptimizer --goflags="--ldflags=-s -w -trimpath -modcacherw"

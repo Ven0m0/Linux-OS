0cafa8db4588d2e4eacb98acd9537d8c59a6a753
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-02 00:18:45 +0100

    Update Debloat.sh

--- RaspberryPi/Scripts/Debloat.sh
+++ RaspberryPi/Scripts/Debloat.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
-[[ $EUID -eq 0 ]] || {
-  sudo "${0/#.\//$PWD/}" || {
-    echo "Administrator privileges required"
-    exit 1
-  }
-  exit
-}
 export HOME="/home/${SUDO_USER:-$USER}" LC_ALL=C LANG=C
+sudo -v
+
 
 sudo apt-get purge -y libreoffice*
-
 echo "Tweaks from https://privacy.sexy"
-sudo apt-get purge -y reportbug
-sudo apt-get purge -y python3-reportbug
-sudo apt-get purge -y reportbug-gtk
-sudo apt-get purge -y popularity-contest
+sudo apt-get purge -y reportbug python3-reportbug reportbug-gtk apport whoopsie popularity-contest
 echo '--- Disable participation in Popularity Contest'
 if [[ -f /etc/popularity-contest.conf ]]; then
   sudo sed -i '/^PARTICIPATE=/d;$aPARTICIPATE=no' "/etc/popularity-contest.conf"

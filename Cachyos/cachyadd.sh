5045495bcac9698a36deec96de311c9abfa4670d
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-04 00:36:07 +0100

    Delete Cachyos/cachyadd.sh

--- Cachyos/cachyadd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Add cachyos repo's to your existing arch install automatically
-# https://wiki.cachyos.org/features/optimized_repos/
-curl https://mirror.cachyos.org/cachyos-repo.tar.xz -O && {
-  tar xvf cachyos-repo.tar.xz \
-    && cd cachyos-repo || exit
-  chmod +x cachyos-repo.sh \
-    && sudo ./cachyos-repo.sh
-}

105f21c158913b9024a32e97e94f5d4c93120159
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-11 07:02:10 +0100

    Update media-optimize.sh

--- Scripts/media-optimize.sh
+++ Scripts/media-optimize.sh
@@ -31,7 +31,7 @@ cache_tool(){
 }
 
 # Pre-cache critical tools
-for tool in fd:fdfind rg:grep sk:fzf eza:ls parallel xargs ffzap ffmpeg; do
+for tool in fd:fdfind:find rg:grep sk:fzf eza:ls rust-parallel:parallel:xargs ffzap:ffmpeg; do
   IFS=: read -r name fallback <<<"$tool"
   cache_tool "$name" "$fallback" || :
 done

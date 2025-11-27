3e3322f0e52c2553975d83510d696b9798834e0e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-27 12:28:59 +0100

    Delete Cachyos/Scripts/WIP/increase-lockout-limit.sh

--- Cachyos/Scripts/WIP/increase-lockout-limit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Increase lockout limit to 10 and decrease timeout to 2 minutes
-sudo sed -i 's|^\(auth\s\+required\s\+pam_faillock.so\)\s\+preauth.*$|\1 preauth silent deny=10 unlock_time=120|' "/etc/pam.d/system-auth"
-sudo sed -i 's|^\(auth\s\+\[default=die\]\s\+pam_faillock.so\)\s\+authfail.*$|\1 authfail deny=10 unlock_time=120|' "/etc/pam.d/system-auth"

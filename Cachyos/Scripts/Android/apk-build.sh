3b0f1d2638bbeefdeebf50cad444712a358713dd
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-13 01:42:53 +0100

    Update apk-build.sh

--- Cachyos/Scripts/Android/apk-build.sh
+++ Cachyos/Scripts/Android/apk-build.sh
@@ -2,29 +2,25 @@
 # shellcheck enable=all shell=bash source-path=SCRIPTDIR external-sources=true
 set -euo pipefail; shopt -s nullglob globstar
 IFS=$'\n\t' LC_ALL=C
-
 apk="$1"
 tmp="workdir"
 out="optimized.apk"
 
 rm -rf "$tmp"
 unzip "$apk" -d "$tmp" &>/dev/null
-
-# strip unused densities
-for d in drawable-xxhdpi drawable-xxxhdpi; do
+# (resources minification)
+for d in drawable-xxhdpi drawable-xxxhdpi values-fr; do
   rm -rf "$tmp/res/$d"
 done
-rm -rf app_decoded/res/drawable-xxhdpi/
-rm -rf app_decoded/res/values-fr/
-
-# recompress images
-find "$tmp/res" -type f -name '*.png' -print0 | xargs -0 zopflipng -m
-
-zstd -19 assets/large.png -o compressed
-d8 --release --output optimized.dex classes.dex
-
+for rem in "$tmp"/res/{drawable-*,values-*}; do
+  [[ "$rem" =~ -en|-zh ]] && rm -rf "$rem"
+done
+# recompress PNGs
+find "$tmp/res" -type f -name '*.png' -print0 | xargs -0 zopflipng -m || :
+# (optional) recompress other large assets with zstd
+find "$tmp/assets" -type f -print0 | xargs -0 -I{} sh -c 'zstd -19 "{}" -o "{}.zst" && mv "{}.zst" "{}"' || :
+# rebuild & align
 apktool b "$tmp" -o new.apk
-# zipalign
-zipalign -fpv 4 "$tmp" "$out"
+zipalign -f -p 4 new.apk "$out"
 # sign
 apksigner sign --ks mykeystore "$out"

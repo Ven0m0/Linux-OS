8d2fd7391c78dabc7cab2ec6104a24ef78f71536
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-24 14:47:06 +0100

    Create ssh.sh

---
+++ Cachyos/ssh.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR
+set -euo pipefail
+shopt -s nullglob globstar
+
+EMAIL="${GIT_AUTHOR_EMAIL:-$(git config get user.email)}"
+
+mkdir -p ~/.ssh && chmod -R 700 ~/.ssh
+ssh-keygen -t ed25519 -C "$EMAIL" -f ~/.ssh/id_git -q
+eval "$(ssh-agent -s)" || eval (ssh-agent -cs)
+ssh-add ~/.ssh/id_git
+
+cat ~/.ssh/id_git.pub | wl-copy -r || cat ~/.ssh/id_git.pub | xclip -sel clipboard
+xdg-open 
+echo "copy to to https://github.com/settings/keys and https://gitlab.com/-/user_settings/ssh_keys"
+
+ssh-keyscan -H github.com gitlab.com >>~/.ssh/known_hosts 
+
+echo "Test GitHub connection"
+ssh -T git@github.com
+echo "Test GitLab connection"
+ssh -T git@gitlab.com

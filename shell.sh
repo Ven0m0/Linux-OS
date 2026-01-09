47edad8500d4ca4c532eb495a40af0f175362336
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-01-09 12:04:51 +0100

    Delete shell.sh

--- shell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck enable=all shell=bash source-path=SCRIPTDIR
-set -euo pipefail
-shopt -s nullglob globstar
-IFS=$'\n\t' LC_ALL=C
-
-# Git safe directory configuration for CI/CD and local environments
-git config --global --add safe.directory "${GITHUB_WORKSPACE:-$PWD}"
-git config --global --add safe.directory "$PWD"
